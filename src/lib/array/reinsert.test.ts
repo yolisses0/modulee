0d5ebdac5135a3b24094ea272c0aaf92e681e752
Yolisses <yolisses0@gmail.com>
2024-12-28 15:27:25 -0300

    Create reinsert

---
+++ src/lib/array/reinsert.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest';
+import { reinsert } from './reinsert';
+import type { Remotion } from './remotion';
+
+test('reinsert', () => {
+	const items = ['1', '3'];
+	const remotion: Remotion<string> = { index: 1, item: '2' };
+
+	reinsert(items, remotion);
+
+	expect(items).toEqual(['1', '2', '3']);
+});
