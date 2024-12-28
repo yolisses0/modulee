d3f3e4c11a0d005c44d1c8f7d768bb9535758bdb
Yolisses <yolisses0@gmail.com>
2024-12-28 17:03:49 -0300

    Create pointer event handlers in NodeItem

--- src/lib/nodes/NodeItem.svelte
+++ src/lib/nodes/NodeItem.svelte
@@ -5,6 +5,8 @@
 	import { RemoveNodeCommand } from './commands/RemoveNodeCommand';
 
 	let { node, space, editor }: { node: Node; space: Space; editor: Editor } = $props();
+	let pointerId: number | undefined = $state();
+	let element: HTMLElement = $state();
 
 	const screenSize = space.getScreenSize(node.size);
 	const screenPosition = space.getScreenPosition(node.position);
@@ -17,11 +19,35 @@
 		editor.execute(removeNodeCommand);
 		return false;
 	}
+
+	function handlePointerDown(e: PointerEvent) {
+		pointerId = e.pointerId;
+		element.setPointerCapture(pointerId);
+		console.log('capture');
+	}
+
+	function handlePointerMove(e: PointerEvent) {
+		if (pointerId) {
+			console.log(e.clientX, e.clientY);
+		}
+	}
+
+	function handlePointerUp(e: PointerEvent) {
+		if (!pointerId) return;
+		element.releasePointerCapture(pointerId);
+		pointerId = undefined;
+
+		console.log('release');
+	}
 </script>
 
 <button
+	bind:this={element}
 	onclick={handleClick}
+	onpointerup={handlePointerUp}
 	oncontextmenu={handleContextMenu}
+	onpointermove={handlePointerMove}
+	onpointerdown={handlePointerDown}
 	style:width={screenSize.x + 'px'}
 	style:height={screenSize.y + 'px'}
 	style:top={screenPosition.y + 'px'}
