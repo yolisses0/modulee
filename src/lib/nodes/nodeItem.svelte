2bbeecf05d5ba45000e4ef364ca6c2d97199a8a7
Yolisses <yolisses0@gmail.com>
2024-12-28 11:47:54 -0300

    Use button instead of div in NodeItem

--- src/lib/nodes/nodeItem.svelte
+++ src/lib/nodes/nodeItem.svelte
@@ -6,14 +6,19 @@
 
 	const screenPosition = space.getScreenPosition(node.position);
 	const screenSize = space.getScreenSize(node.size);
+
+	function handleClick(e: MouseEvent) {
+		console.log('here');
+	}
 </script>
 
-<div
+<button
+	onclick={handleClick}
 	style:width={screenSize.x + 'px'}
 	style:height={screenSize.y + 'px'}
 	style:top={screenPosition.y + 'px'}
 	style:left={screenPosition.x + 'px'}
-	class="absolute w-fit border"
+	class="absolute w-fit bg-gray-500"
 >
 	{node.id}
-</div>
+</button>
