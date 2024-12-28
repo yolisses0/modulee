f7ce538f51cbff18eed419cc090a3bacc6167d68
Yolisses <yolisses0@gmail.com>
2024-12-28 11:18:11 -0300

    Add operations in Vector

--- src/lib/space/vector.ts
+++ src/lib/space/vector.ts
@@ -4,7 +4,39 @@ export class Vector {
         public y: number
     ) { }
 
+    static zero() {
+        return new Vector(0, 0)
+    }
+
     static one() {
         return new Vector(1, 1)
+    }
+
+    add(vector: Vector) {
+        return new Vector(
+            this.x + vector.x,
+            this.y + vector.y,
+        )
+    }
+
+    subtract(vector: Vector) {
+        return new Vector(
+            this.x - vector.x,
+            this.y - vector.y,
+        )
+    }
+
+    multiply(vector: Vector) {
+        return new Vector(
+            this.x * vector.x,
+            this.y * vector.y,
+        )
+    }
+
+    divide(vector: Vector) {
+        return new Vector(
+            this.x / vector.x,
+            this.y / vector.y,
+        )
     }
 }
