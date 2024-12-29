fb031132ff3f0d130579a08ea4a2b889aea8d177
Yolisses <yolisses0@gmail.com>
2024-12-29 11:04:25 -0300

    Make Node a class

--- src/lib/node/Node.ts
+++ src/lib/node/Node.ts
@@ -1,9 +1,13 @@
 import type { Vector } from '$lib/space/Vector';
 import type { Connector } from './connector/Connector';
+import { defaultNodeSize } from './defaultNodeSize';
 
-export type Node = {
-	id: string;
-	size: Vector;
-	position: Vector;
-	connectors: Connector[];
-};
+export class Node {
+	connectors: Connector[] = [];
+	size: Vector = defaultNodeSize;
+
+	constructor(
+		public id: string,
+		public position: Vector,
+	) {}
+}
