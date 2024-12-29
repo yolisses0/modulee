fb1efa60b58b4cc57bd773498f3831bef94c9d0a
Yolisses <yolisses0@gmail.com>
2024-12-29 10:42:35 -0300

    Create getVectorStringPx and getVectorsStringPx

--- src/lib/node/NodeListBackground.svelte
+++ src/lib/node/NodeListBackground.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Space } from '$lib/space/Space';
 	import { Vector } from '$lib/space/Vector';
+	import { getVectorStringPx } from '$lib/wire/utils/getVectorStringPx';
 
 	let { space }: { space: Space } = $props();
 
@@ -10,7 +11,7 @@
 <div
 	class="pointer-events-none absolute h-full w-full"
 	style="
-    background-size: {gridSize.x}px {gridSize.y}px;
-    background-position: {gridSize.x / 2}px {gridSize.x / 2}px;
+    background-size: {getVectorStringPx(gridSize)};
+    background-position: {getVectorStringPx(gridSize.divide(Vector.fromNumber(2)))};
     background-image: radial-gradient(circle, #fff5 1px, rgba(0, 0, 0, 0) 1px);"
 ></div>
