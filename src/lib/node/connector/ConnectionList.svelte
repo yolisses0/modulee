c892a29a4d412493996cbda8bcec7e9d3b25c4a9
Yolisses <yolisses0@gmail.com>
2024-12-30 16:24:45 -0300

    Create ConnectionList

---
+++ src/lib/node/connector/ConnectionList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Space } from '$lib/space/Space';
+	import type { Node } from '../Node.svelte';
+	import ConnectionItem from './ConnectionItem.svelte';
+
+	const { nodes, space }: { space: Space; nodes: Node[] } = $props();
+</script>
+
+{#each nodes as node (node.id)}
+	{#each node.connectors as connector (connector.id)}
+		{#if connector.connectedTo}
+			<ConnectionItem {space} startConnector={connector} endConnector={connector.connectedTo} />
+		{/if}
+	{/each}
+{/each}
