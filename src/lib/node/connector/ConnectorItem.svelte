d800ce48368d85ac3a3ccbfdd1a6960653329dd2
Yolisses <yolisses0@gmail.com>
2024-12-30 15:02:49 -0300

    Use em in ConnectorItem style

--- src/lib/node/connector/ConnectorItem.svelte
+++ src/lib/node/connector/ConnectorItem.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
 	import type { Space } from '$lib/space/Space';
-	import { getScreenUnit } from '$lib/utils/getScreenUnit';
 	import type { Connector } from './Connector';
 
 	let { space, connector }: { space: Space; connector: Connector } = $props();
-
-	const screenUnit = getScreenUnit(space);
 </script>
 
 <div class="flex-row items-center whitespace-nowrap">
 	<div
 		class="shrink-0 rounded-full bg-green-500"
-		style:width={(3 / 4) * screenUnit.x + 'px'}
-		style:height={(3 / 4) * screenUnit.y + 'px'}
-		style:margin={(1 / 2 / 4) * screenUnit.x + 'px'}
+		style:width="0.9em"
+		style:height="0.9em"
+		style:margin="0.05em"
 	></div>
 	<div>{connector.name}</div>
 </div>
