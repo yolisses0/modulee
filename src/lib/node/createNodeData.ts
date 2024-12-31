52a82af8bfbd133d6e95f81711892918f341e526
Yolisses <yolisses0@gmail.com>
2024-12-31 07:09:50 -0300

    Add connectors in createNodeData

--- src/lib/node/createNodeData.ts
+++ src/lib/node/createNodeData.ts
@@ -6,7 +6,11 @@ import type { NodeData } from './NodeData';
 export function createNodeData(position: Vector): NodeData {
 	return {
 		id: createId(),
-		connectors: [],
+		connectors: [
+			{ id: createId(), name: 'connector1' },
+			{ id: createId(), name: 'connector2' },
+			{ id: createId(), name: 'connector3' },
+		],
 		size: defaultNodeSize,
 		position: position.subtract(defaultNodeSize.divide(Vector.fromNumber(2))),
 	};
