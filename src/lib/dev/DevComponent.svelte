5fc63dce2173b4f2869110075cba9e84a60c8468
Yolisses <yolisses0@gmail.com>
2025-01-01 05:24:03 -0300

    Experiment with contexts in DevPage

---
+++ src/lib/dev/DevComponent.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	const devValue = getContext('devValue');
+</script>
+
+<div>
+	{devValue.foo}
+</div>
--- src/lib/dev/DevPage.svelte
+++ src/lib/dev/DevPage.svelte
@@ -1,4 +1,10 @@
-<div style="font-size: 50%;">
-	<div class="bg-red-500 p-2 text-sm">text1</div>
-	<div style="font-size:  0.875em;  padding: 0.5em;" class="bg-blue-500">text2</div>
-</div>
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import DevComponent from './DevComponent.svelte';
+
+	const devValue = { foo: 5 };
+
+	setContext('devValue', devValue);
+</script>
+
+<DevComponent />
