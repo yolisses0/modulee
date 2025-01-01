bcc13abc5b6c73d1f46937f9809b76d5c1126de3
Yolisses <yolisses0@gmail.com>
2025-01-01 16:57:23 -0300

    Create DevUnitDiv

---
+++ src/lib/node/dev/DevUnitDiv.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Space } from '$lib/space/Space';
+	import { Vector } from '$lib/space/Vector';
+
+	interface Props {
+		space: Space;
+	}
+
+	const { space }: Props = $props();
+
+	const screenPosition = space.getScreenPosition(Vector.zero());
+	const screenSize = space.getScreenSize(Vector.one());
+</script>
+
+<div
+	id="DevUnitDiv"
+	class="absolute bg-blue-500"
+	style:width={screenSize.x + 'px'}
+	style:height={screenSize.y + 'px'}
+	style:top={screenPosition.y + 'px'}
+	style:left={screenPosition.x + 'px'}
+></div>
