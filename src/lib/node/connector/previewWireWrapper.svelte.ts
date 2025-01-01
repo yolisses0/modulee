7780ea71d9599473ea58d00d8752aa5b58ca87c0
Yolisses <yolisses0@gmail.com>
2025-01-01 08:00:12 -0300

    Remove previewWire default value

--- src/lib/node/connector/previewWireWrapper.svelte.ts
+++ src/lib/node/connector/previewWireWrapper.svelte.ts
@@ -1,9 +1,3 @@
-import { Vector } from '$lib/space/Vector';
 import type { Wire } from '$lib/wire/Wire';
 
-export const previewWireWrapper = $state<{ previewWire?: Wire }>({
-	previewWire: {
-		startPosition: new Vector(13, 5),
-		endPosition: new Vector(18, 12),
-	},
-});
+export const previewWireWrapper = $state<{ previewWire?: Wire }>({});
