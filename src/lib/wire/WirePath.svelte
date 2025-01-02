6403ef47b405295d99e38d8a77206c7a4c8f1006
Yolisses <yolisses0@gmail.com>
2025-01-02 16:48:59 -0300

    Style WirePath

--- src/lib/wire/WirePath.svelte
+++ src/lib/wire/WirePath.svelte
@@ -35,5 +35,5 @@
 	}
 </script>
 
-<path d={pathD} class="stroke-green-700" fill="transparent" stroke-width="0.25" />
+<path d={pathD} class="stroke-green-700" fill="transparent" stroke-width="0.3" />
 <path d={pathD} class="stroke-green-500" fill="transparent" stroke-width="0.2" />
