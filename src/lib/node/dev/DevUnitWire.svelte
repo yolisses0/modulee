5a91a71edad238d7feece183b92bd578e1e97828
Yolisses <yolisses0@gmail.com>
2025-01-02 16:44:25 -0300

    Create DevUnitWire

---
+++ src/lib/node/dev/DevUnitWire.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Space } from '$lib/space/Space';
+	import { Vector } from '$lib/space/Vector';
+
+	import Wire from '$lib/wire/Wire.svelte';
+
+	interface Props {
+		space: Space;
+	}
+
+	const { space }: Props = $props();
+</script>
+
+<Wire {space} startPosition={new Vector(0, 0)} endPosition={new Vector(1, 1)} />
