2deb6b53db347cb6819d56e43cd632c0559f4af1
Yolisses <yolisses0@gmail.com>
2025-01-03 07:41:22 -0300

    Fix PreviewWire

--- src/lib/node/input/PreviewWire.svelte
+++ src/lib/node/input/PreviewWire.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Space } from '$lib/space/Space';
 	import Wire from '$lib/wire/Wire.svelte';
+	import { Output } from '../output/Output.svelte';
 	import type { PreviewConnection } from './PreviewConnection';
 
 	interface Props {
@@ -9,11 +10,20 @@
 	}
 
 	const { space, previewConnection }: Props = $props();
+
+	// TODO find a more legible way to do this
+	const [input, output] = $derived.by(function () {
+		const { startConnector, endConnector } = previewConnection;
+		if (startConnector instanceof Output) {
+			return [startConnector, endConnector];
+		} else {
+			return [endConnector, startConnector];
+		}
+	});
 </script>
 
 <Wire
 	{space}
-	endPosition={previewConnection.startConnector.jointPosition}
-	startPosition={previewConnection.endConnector?.jointPosition ??
-		previewConnection.dataPointerPosition}
+	endPosition={output?.jointPosition ?? previewConnection.dataPointerPosition}
+	startPosition={input?.jointPosition ?? previewConnection.dataPointerPosition}
 />
