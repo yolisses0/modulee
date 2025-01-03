bf3da880d0c235f4d310a260bc9c581000a23660
Yolisses <yolisses0@gmail.com>
2025-01-03 14:55:12 -0300

    Use ConnectorItem in OutputItem

--- src/lib/node/output/OutputItem.svelte
+++ src/lib/node/output/OutputItem.svelte
@@ -2,14 +2,10 @@
 	import type { Editor } from '$lib/editor/Editor.svelte';
 	import type { Space } from '$lib/space/Space';
 	import { createId } from '$lib/utils/createId';
-	import { getDataPointerPosition } from '$lib/utils/getDataPointerPosition';
 	import { SetInputConnectedOutput } from '../commands/SetInputConnectedOutput';
-	import JointCircle from '../connector/JointCircle.svelte';
-	import { getContainerContext } from '../containerContext';
-	import { getElementPosition } from '../getElementPosition';
-	import { getPointerPosition } from '../getPointerPosition';
+	import ConnectorItem from '../connector/ConnectorItem.svelte';
 	import { Input } from '../input/Input.svelte';
-	import { getPreviewConnectionContext } from '../input/previewConnectionContext';
+	import type { PreviewConnection } from '../input/PreviewConnection';
 	import type { Output } from './Output.svelte';
 
 	interface Props {
@@ -19,91 +15,22 @@
 	}
 	let { space, output, editor }: Props = $props();
 
-	let containerWrapper = getContainerContext();
+	function handlePreviewEnd(previewConnection: PreviewConnection) {
+		const { endConnector } = previewConnection;
 
-	let previewConnectionWrapper = getPreviewConnectionContext();
-
-	function handlePointerDown(e: PointerEvent) {
-		if (e.pointerType !== 'mouse' || e.button === 1) return;
-		if (!containerWrapper.container) return;
-
-		const containerPosition = getElementPosition(containerWrapper.container);
-		const screenPosition = getPointerPosition(e).subtract(containerPosition);
-		const dataPosition = space.getDataPosition(screenPosition);
-
-		previewConnectionWrapper.previewConnection = {
-			startConnector: output,
-			dataPointerPosition: dataPosition,
-		};
-
-		containerWrapper.container.addEventListener('pointermove', handleContainerPointerMove as any);
-		containerWrapper.container.addEventListener('pointerup', handleContainerPointerUp as any);
-	}
-
-	function handleContainerPointerMove(e: PointerEvent) {
-		if (!containerWrapper.container) return;
-		const dataPointerPosition = getDataPointerPosition(e, space, containerWrapper.container);
-		if (previewConnectionWrapper.previewConnection) {
-			previewConnectionWrapper.previewConnection.dataPointerPosition = dataPointerPosition;
-		}
-	}
-
-	function handleContainerPointerUp(e: PointerEvent) {
-		if (previewConnectionWrapper.previewConnection) {
-			const { endConnector: input } = previewConnectionWrapper.previewConnection;
-
-			if (input instanceof Input) {
-				const command = new SetInputConnectedOutput({
-					id: createId(),
-					type: 'SetInputConnectedOutput',
-					details: { inputId: input.id, outputId: output.id },
-				});
-				editor.execute(command);
-			}
-		}
-
-		previewConnectionWrapper.previewConnection = undefined;
-
-		if (!containerWrapper.container) return;
-		containerWrapper.container.removeEventListener(
-			'pointermove',
-			handleContainerPointerMove as any,
-		);
-		containerWrapper.container.removeEventListener('pointerup', handleContainerPointerUp as any);
-	}
-
-	function handlePointerEnter(e: PointerEvent) {
-		if (!previewConnectionWrapper.previewConnection) return;
-
-		const { startConnector } = previewConnectionWrapper.previewConnection;
-
-		if (startConnector instanceof Input) {
-			previewConnectionWrapper.previewConnection.endConnector = output;
-		}
-	}
-
-	function handlePointerOut(e: PointerEvent) {
-		if (!previewConnectionWrapper.previewConnection) return;
-
-		const { startConnector, endConnector } = previewConnectionWrapper.previewConnection;
-
-		if (startConnector instanceof Input && endConnector === output) {
-			previewConnectionWrapper.previewConnection.endConnector = undefined;
+		if (endConnector instanceof Input && endConnector.connectedOutput !== output) {
+			const command = new SetInputConnectedOutput({
+				id: createId(),
+				type: 'SetInputConnectedOutput',
+				details: { inputId: endConnector.id, outputId: output.id },
+			});
+			editor.execute(command);
 		}
 	}
 </script>
 
-<button
-	class="hover-bg w-full"
-	onpointerout={handlePointerOut}
-	onpointerdown={handlePointerDown}
-	onpointerenter={handlePointerEnter}
->
-	<!-- TODO consider using some other approach to prevent
- children events of pointer out. E.g.: replace pointer events
- by mouse events  -->
-	<div class="pointer-events-none w-full flex-row-reverse items-center whitespace-nowrap">
-		<JointCircle />
-		<div>{output.id.slice(0, 4)}</div>
-	</div>
-</button>
+<!-- This div forces the offset to be given by the 
+ input position instead of the node position -->
+<div class="relative">
+	<ConnectorItem onPreviewEnd={handlePreviewEnd} {space} connector={output} targetClass={Input} />
+</div>
