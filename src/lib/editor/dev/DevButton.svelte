c0b5ae34f4377a73f25a510a5d805e836411a421
Yolisses <yolisses0@gmail.com>
2025-01-13 20:11:14 -0300

    Pass graph to plugin

--- src/lib/editor/dev/DevButton.svelte
+++ src/lib/editor/dev/DevButton.svelte
@@ -6,6 +6,26 @@
 		window.__JUCE__.backend.emitEvent('passSomeString', {
 			someString: 'hello, world',
 		});
+
+		const devNodesData = `[
+    {
+        "id": 1,
+        "type": "ConstantNode",
+        "extras": {
+            "value": 100
+        }
+    },
+    {
+        "id": 2,
+        "type": "OutputNode",
+        "input_ids": {
+            "input": 1
+        }
+    }
+]`;
+		window.__JUCE__.backend.emitEvent('graphChange', {
+			nodesData: devNodesData,
+		});
 	}
 </script>
 
