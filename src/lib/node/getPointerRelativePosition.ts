0e051f27d2588e892655fa50d732120de2388736
Yolisses <yolisses0@gmail.com>
2025-01-17 07:24:44 -0300

    Remove get pointer position and relative position functions

--- src/lib/node/getPointerRelativePosition.ts
+++
@@ -1,8 +0,0 @@
-import { getElementPosition } from './getElementPosition';
-import { getPointerPosition } from './getPointerPosition';
-
-export function getPointerScreenPosition(e: PointerEvent, element: Element) {
-	const pointerPosition = getPointerPosition(e);
-	const boundingRectangle = getElementPosition(element);
-	return pointerPosition.subtract(boundingRectangle);
-}
