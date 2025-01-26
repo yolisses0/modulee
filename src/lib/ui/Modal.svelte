44d5c542f8b89cacd6b4a0adf2d0b3339cb3f54e
Yolisses <yolisses0@gmail.com>
2025-01-26 10:34:40 -0300

    Fix aria-label of Modal overlay

--- src/lib/ui/Modal.svelte
+++ src/lib/ui/Modal.svelte
@@ -9,7 +9,7 @@
 	const { children, closeModal }: Props = $props();
 </script>
 
-<button class="fixed inset-0 bg-black/25" onclick={closeModal}></button>
+<button class="fixed inset-0 bg-black/25" onclick={closeModal} aria-label="Close modal"></button>
 
 <div class="pointer-events-none fixed inset-0 items-center justify-center">
 	<div class="pointer-events-auto contents">
