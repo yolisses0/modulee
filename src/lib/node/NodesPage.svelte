e8f2e77300b96b667f6bb30c0ff135ca16a37969
Yolisses <yolisses0@gmail.com>
2025-02-01 10:38:25 -0300

    Create NodesToolbar

--- src/lib/node/NodesPage.svelte
+++ src/lib/node/NodesPage.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
 	import { getEditorContext } from '$lib/editor/editorContext';
-	import RedoButton from '$lib/editor/RedoButton.svelte';
-	import UndoButton from '$lib/editor/UndoButton.svelte';
-	import BackButton from '$lib/group/BackButton.svelte';
 	import { getGroupIdContext } from '$lib/group/groupIdContext';
-	import GroupNodesButton from '$lib/group/GroupNodesButton.svelte';
 	import { OffsetConverter } from '$lib/space/OffsetConverter';
 	import { Space } from '$lib/space/Space.js';
 	import { setSpaceContext } from '$lib/space/spaceContext';
 	import { ZoomConverter } from '$lib/space/ZoomConverter';
-	import ZoomInButton from '$lib/zoom/ZoomInButton.svelte';
-	import ZoomOutButton from '$lib/zoom/ZoomOutButton.svelte';
 	import { Vector } from 'nodes-editor';
 	import type { Snippet } from 'svelte';
 	import NodeList from './NodeList.svelte';
+	import NodesToolbar from './NodesToolbar.svelte';
 
 	interface Props {
 		topBarChildren?: Snippet;
@@ -42,15 +37,7 @@
 </script>
 
 <div class="flex h-screen w-screen flex-col overflow-hidden">
-	<div class="flex flex-row">
-		{@render topBarChildren?.()}
-		<UndoButton />
-		<RedoButton />
-		<ZoomInButton bind:zoom />
-		<ZoomOutButton bind:zoom />
-		<GroupNodesButton />
-		<BackButton />
-	</div>
+	<NodesToolbar bind:zoom {topBarChildren} />
 	<div class="flex-1 overflow-auto">
 		<NodeList nodes={visibleNodes} />
 	</div>
---
+++ src/lib/node/NodesToolbar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import RedoButton from '$lib/editor/RedoButton.svelte';
+	import UndoButton from '$lib/editor/UndoButton.svelte';
+	import BackButton from '$lib/group/BackButton.svelte';
+	import GroupNodesButton from '$lib/group/GroupNodesButton.svelte';
+	import ZoomInButton from '$lib/zoom/ZoomInButton.svelte';
+	import ZoomOutButton from '$lib/zoom/ZoomOutButton.svelte';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		zoom: number;
+		topBarChildren?: Snippet;
+	}
+
+	let { zoom = $bindable(), topBarChildren }: Props = $props();
+</script>
+
+<div class="flex flex-row border-b border-black">
+	{@render topBarChildren?.()}
+	<UndoButton />
+	<RedoButton />
+	<ZoomInButton bind:zoom />
+	<ZoomOutButton bind:zoom />
+	<GroupNodesButton />
+	<BackButton />
+</div>
