fbbcdcf1b4c96578cf69a6915f4817acb9b1f6e8
Yolisses <yolisses0@gmail.com>
2025-02-01 20:52:00 -0300

    Prevent line breaking in AddNodeMenu

--- src/lib/node/add/AddNodeMenu.svelte
+++ src/lib/node/add/AddNodeMenu.svelte
@@ -40,7 +40,7 @@
 
 <div class="flex max-h-[75vh] flex-col rounded bg-zinc-700">
 	<div class="border-b border-black/25 p-2">Add node</div>
-	<div class="scroll-small flex flex-col overflow-auto">
+	<div class="scroll-small flex flex-col overflow-auto whitespace-nowrap">
 		<NodeTypeList nodeTypes={devNodeTypes} onTypeClick={handleTypeClick} />
 	</div>
 </div>
