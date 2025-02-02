7de6f5a70fc914b9d0e70711278a3db3e9b3aaa7
Yolisses <yolisses0@gmail.com>
2025-02-02 15:39:48 -0300

    Create play/pause button on DevPage

--- src/dev/DevPage.svelte
+++ src/dev/DevPage.svelte
@@ -1,16 +1,34 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	let audioContext = $state<AudioContext>();
+	let isPlaying = $state(false);
+
 	onMount(async () => {
 		const response = await fetch('modulee_engine_wasm_bg.wasm');
 		const bytes = await response.arrayBuffer();
-		const audioContext = new AudioContext();
+
+		audioContext = new AudioContext();
 		await audioContext.audioWorklet.addModule('engine-processor.js');
 		const engineNode = new AudioWorkletNode(audioContext, 'engine-processor', {
 			processorOptions: { bytes },
 		});
 		engineNode.connect(audioContext.destination);
+
+		isPlaying = audioContext.state === 'running';
 	});
+
+	function handleClick() {
+		if (!audioContext) return;
+		if (isPlaying) {
+			audioContext.suspend();
+		} else {
+			audioContext.resume();
+		}
+		isPlaying = !isPlaying;
+	}
 </script>
 
-hello, dev
+<button onclick={handleClick}>
+	{isPlaying ? 'pause' : 'play'}
+</button>
