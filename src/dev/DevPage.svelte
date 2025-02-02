de061b788427cc4967834287c14face295904aa9
Yolisses <yolisses0@gmail.com>
2025-02-02 09:27:07 -0300

    Use process_block in DevPage

--- src/dev/DevPage.svelte
+++ src/dev/DevPage.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
-	import { greet } from 'modulee-engine-wasm';
+	import { Graph, initialize_logging, set_panic_hook } from 'modulee-engine-wasm';
+	import { onMount } from 'svelte';
 
 	function handleClick() {
-		greet();
+		const graph = new Graph();
+
+		const buffer = new Float32Array(100);
+		graph.process_block(buffer, buffer.length);
+		console.log(buffer);
 	}
+
+	onMount(() => {
+		set_panic_hook();
+		initialize_logging();
+	});
 </script>
 
-<button class="common-button" onclick={handleClick}> Greet </button>
+<button class="common-button" onclick={handleClick}> Click me </button>
