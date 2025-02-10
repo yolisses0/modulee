1850ed6cef77b11908643ec86e811628d51e6e5e
Yolisses <yolisses0@gmail.com>
2025-02-09 21:05:00 -0300

    Remove script tag created by mistake in NodeItem

--- src/lib/node/NodeItem.svelte
+++ src/lib/node/NodeItem.svelte
@@ -24,42 +24,30 @@
 </script>
 
 <BaseNodeItem {node} position={screenPosition}>
-	<ConnectorArea
-		connectorId={node.output.id}
-		endConnectorCondition={connectorCondition.endConnectorCondition}
+	<div
+		style:width="4lh"
+		style:outline-width="0.1lh"
+		style:border-radius="0.4lh"
+		class:outline-blue-500={isSelected}
+		class:outline-zinc-700={!isSelected}
+		class="flex flex-col bg-zinc-600 outline"
 	>
-		<div
-			style:width="4lh"
-			style:outline-width="0.1lh"
-			style:border-radius="0.4lh"
-			class:outline-blue-500={isSelected}
-			class:outline-zinc-700={!isSelected}
-			class="flex flex-col bg-zinc-600 outline"
+		<NodeItemHeader {node} />
+		<ConnectorArea
+			connectorId={node.output.id}
+			endConnectorCondition={connectorCondition.endConnectorCondition}
 		>
-			<NodeItemHeader {node} />
-			<script lang="ts">
-				import type { Input } from '$lib/data/Input.svelte.js';
-				import { ConnectorArea } from 'nodes-editor';
-				import ConnectorJoint from './ConnectorJoint.svelte';
-				import { endConnectorCondition } from './endConnectorCondition.js';
-
-				interface Props {
-					input: Input;
-				}
-
-				const { input }: Props = $props();
-			</script>
-
 			<!-- TODO move it to other file -->
 			<div class="relative flex flex-row items-center hover:bg-white/10">
 				output
 				<ConnectorJoint connector={node.output} />
 			</div>
+		</ConnectorArea>
 
-			{#each node.inputs as input (input.name)}
-				<InputItem {input} />
-			{/each}
-			{@render children?.()}
-		</div>
-	</ConnectorArea>
+		{#each node.inputs as input (input.name)}
+			<InputItem {input} />
+		{/each}
+
+		{@render children?.()}
+	</div>
 </BaseNodeItem>
