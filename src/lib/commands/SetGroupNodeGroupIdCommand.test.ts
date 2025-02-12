c6e13fb493655f057e77e6af0c3c4e5733deb2ee
Yolisses <yolisses0@gmail.com>
2025-02-12 15:11:01 -0300

    Fix SetGroupNodeGroupIdCommand test

--- src/lib/commands/SetGroupNodeGroupIdCommand.test.ts
+++ src/lib/commands/SetGroupNodeGroupIdCommand.test.ts
@@ -9,19 +9,21 @@ test('SetGroupNodeGroupIdCommand', () => {
 	const editorData = {
 		nodes: new ById([
 			{ id: 'node1' },
-			{ id: 'node2', type: 'GroupNode', extras: { groupId: 1 } as ExtrasData },
+			{ id: 'node2', type: 'GroupNode', extras: { groupId: 'group1' } as ExtrasData },
 			{ id: 'node3' },
 		]),
 	} as EditorData;
-	const command = new SetGroupNodeGroupIdCommand(mockCommandData({ nodeId: 'node2', groupId: 2 }));
+	const command = new SetGroupNodeGroupIdCommand(
+		mockCommandData({ nodeId: 'node2', groupId: 'group2' }),
+	);
 
 	command.execute(editorData);
 
-	expect(editorData.nodes.get('node2').extras.groupId).toBe(2);
+	expect(editorData.nodes.get('node2').extras.groupId).toBe('group2');
 
 	command.undo(editorData);
 
-	expect(editorData.nodes.get('node2').extras.groupId).toBe(1);
+	expect(editorData.nodes.get('node2').extras.groupId).toBe('group1');
 });
 
 test('SetGroupNodeGroupIdCommand with wrong type', () => {
@@ -32,7 +34,9 @@ test('SetGroupNodeGroupIdCommand with wrong type', () => {
 			{ id: 'node3' },
 		]),
 	} as EditorData;
-	const command = new SetGroupNodeGroupIdCommand(mockCommandData({ nodeId: 'node3', groupId: 2 }));
+	const command = new SetGroupNodeGroupIdCommand(
+		mockCommandData({ nodeId: 'node3', groupId: 'group1' }),
+	);
 
 	expect(() => {
 		command.execute(editorData);
