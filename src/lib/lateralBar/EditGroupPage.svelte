88f6a9e3eab7cac46e84ec9eb68496c048b9ec08
Yolisses <yolisses0@gmail.com>
2025-02-12 18:08:48 -0300

    Add fields to EditGroupPage

--- src/lib/lateralBar/EditGroupPage.svelte
+++ src/lib/lateralBar/EditGroupPage.svelte
@@ -8,4 +8,20 @@
 	const group = $derived(editorContext.editor.groups.get(groupContext.groupId));
 </script>
 
-<input type="text" value={group.name} class="rounded bg-transparent p-2" />
+<div class="flex flex-col items-stretch gap-2 p-2">
+	<label class="flex flex-col">
+		Name
+		<input
+			type="text"
+			value={group.name}
+			class="rounded border border-white/10 bg-transparent p-2"
+		/>
+	</label>
+	<label class="flex flex-col">
+		Type
+		<select class="hover-bg rounded border border-white/10 bg-zinc-900 p-2">
+			<option class="bg-zinc-800" value="common">Common</option>
+			<option class="bg-zinc-800" value="common">Voice</option>
+		</select>
+	</label>
+</div>
