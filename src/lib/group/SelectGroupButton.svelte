d74d462fad13493977665e352f1bf26ba160d387
Yolisses <yolisses0@gmail.com>
2025-02-12 16:46:03 -0300

    Create select group menu with portal

--- src/lib/group/SelectGroupButton.svelte
+++ src/lib/group/SelectGroupButton.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { getRootElementContext } from 'nodes-editor';
+	import Portal from 'svelte-portal';
 	import SelectGroupMenuWrapper from './SelectGroupMenuWrapper.svelte';
 
 	interface Props {
@@ -9,17 +11,28 @@
 
 	let mouseEvent = $state<MouseEvent>();
 
-	function handleClick(e: MouseEvent) {
+	function handlePointerDown(e: PointerEvent) {
 		e.stopPropagation();
+	}
+
+	function handleClick(e: MouseEvent) {
 		mouseEvent = e;
 	}
+
+	const rootElementContext = getRootElementContext();
 </script>
 
 <button
 	onclick={handleClick}
 	style:padding-inline="0.25lh"
-	class="overflow-and-ellipsis opacity-50"
+	onpointerdown={handlePointerDown}
+	class="hover-bg overflow-and-ellipsis"
 >
-	Select group
+	<span class="opacity-50">Select group</span>
 </button>
-<SelectGroupMenuWrapper {mouseEvent} {groupNodeId} />
+
+{#if rootElementContext.rootElement}
+	<Portal target={rootElementContext.rootElement as HTMLElement}>
+		<SelectGroupMenuWrapper {mouseEvent} {groupNodeId} />
+	</Portal>
+{/if}
