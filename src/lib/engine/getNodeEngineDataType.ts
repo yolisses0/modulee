615b92213b6c1232c93a662610da00fd063c8776
Yolisses <yolisses0@gmail.com>
2025-02-12 21:36:51 -0300

    Fix getNodeEngineDataType

--- src/lib/engine/getNodeEngineDataType.ts
+++ src/lib/engine/getNodeEngineDataType.ts
@@ -1,10 +1,14 @@
 import { findById } from '$lib/array/findById';
 import type { Group } from '$lib/data/Group.svelte';
+import { GroupNode } from '$lib/data/GroupNode.svelte';
 import type { Node } from '$lib/data/Node.svelte';
 
 export function getNodeEngineDataType(node: Node, groups: Group[]) {
-	if (node.type === 'GroupNode') {
-		const group = findById(groups, node.groupId);
+	if (node instanceof GroupNode) {
+		if (!node.targetGroupId) {
+			throw new Error('Not implemented for undefined target group id');
+		}
+		const group = findById(groups, node.targetGroupId);
 		if (group.type === 'voice') {
 			return 'GroupVoicesNode';
 		}
