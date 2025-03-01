17d6f7b3d8c96c8819d6d344970360dd53d5fc02
Yolisses <yolisses0@gmail.com>
2025-03-01 11:47:04 -0300

    Allow new device connections in WebMidiBackend

--- src/lib/engine/WebMidiBackend.ts
+++ src/lib/engine/WebMidiBackend.ts
@@ -8,11 +8,27 @@ export class WebMidiBackend {
 
 	async initialize() {
 		this.webMidi = await WebMidi.enable();
-		this.webMidi.inputs.forEach((device) => {
+		// Brute force of keeping stuff updated.
+		//
+		// TODO find a more elegant solution, maybe allowing users to select
+		// devices.
+		this.webMidi.addListener('midiaccessgranted', this.updateDeviceListeners);
+		this.webMidi.addListener('connected', this.updateDeviceListeners);
+		this.webMidi.addListener('disabled', this.updateDeviceListeners);
+		this.updateDeviceListeners();
+	}
+
+	updateDeviceListeners = () => {
+		// Removes the possible previous listeners to prevent duplicate event
+		// handling
+		this.webMidi?.inputs.forEach((device) => {
+			device.removeListener('noteon', this.onNoteOn);
 			device.addListener('noteon', this.onNoteOn);
+
+			device.removeListener('noteoff', this.onNoteOff);
 			device.addListener('noteoff', this.onNoteOff);
 		});
-	}
+	};
 
 	async destroy() {
 		this.webMidi?.disable();
