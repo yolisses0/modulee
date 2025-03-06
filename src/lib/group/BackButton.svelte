e2d754701f399414ce2cf496e27ebd0f2e242ac2
Yolisses <yolisses0@gmail.com>
2025-03-06 07:49:57 -0300

    Fix BackButton

--- src/lib/group/BackButton.svelte
+++ src/lib/group/BackButton.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
-	import { page } from '$app/state';
 	import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
-	let isActive = $state(false);
-	let url = $state(page.url.pathname);
-
-	// Receives a url like "/projects/projectId/.../groupIdN/"
-	// and returns "/projects/projectId/.../groupIdN-1/"
-	$effect(() => {
-		const parts = page.url.pathname.split('/').filter(Boolean);
-		isActive = parts.length > 2;
-		if (isActive) {
-			parts.pop();
-		}
-		url = '/' + parts.join('/');
-	});
+	function handleClick() {
+		history.back();
+	}
 </script>
 
-<a
-	href={url}
-	aria-disabled={!isActive}
-	class:disabled={!isActive}
-	class="common-button"
-	title="Previous page (Alt+←)"
->
+<button class="common-button" onclick={handleClick} title="Previous page (Alt+←)">
 	<Fa icon={faArrowLeft} />
-</a>
+</button>
