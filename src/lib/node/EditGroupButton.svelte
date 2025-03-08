9ead2d4efb7fc7b90388f288f14314e52edb800a
Yolisses <yolisses0@gmail.com>
2025-03-08 19:53:03 -0300

    Fix EditGroupButton href

--- src/lib/node/EditGroupButton.svelte
+++ src/lib/node/EditGroupButton.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { page } from '$app/state';
 	import type { Group } from '$lib/data/Group.svelte';
+	import { getProjectDataContext } from '$lib/project/projectDataContext';
 
 	interface Props {
 		group: Group;
 	}
 
 	const { group }: Props = $props();
+	const projectDataContext = getProjectDataContext();
+	const href = $derived(`/projects/${projectDataContext.projectData.id}/groups/${group.id}`);
 
 	function handlePointerDown(e: PointerEvent) {
 		e.stopPropagation();
@@ -14,9 +16,9 @@
 </script>
 
 <a
+	{href}
 	draggable="false"
 	title="Edit group"
-	href="{page.url}/{group.id}"
 	style:padding-inline="0.25lh"
 	onpointerdown={handlePointerDown}
 	class="overflow-and-ellipsis hover-bg flex flex-1 flex-row items-center text-blue-300"
