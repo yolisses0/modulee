d684f9e283b7c873773bf74b55a73190a806581e
Yolisses <yolisses0@gmail.com>
2025-03-09 08:54:51 -0300

    Refactor ExportProjectButton

--- src/lib/lateralBar/ExportProjectButton.svelte
+++ src/lib/lateralBar/ExportProjectButton.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
-	import { getGraphRegistryContext } from '$lib/graph/graphRegistryContext';
 	import { getProjectDataContext } from '$lib/project/projectDataContext';
 	import { faFileExport } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 	import { downloadJson } from './downloadJson';
 
-	const graphRegistryContext = getGraphRegistryContext();
 	const projectDataContext = getProjectDataContext();
 
 	function handleClick() {
 		const { projectData } = projectDataContext;
-
-		// DEBUG
-		// const fileName = projectData.name + '.modulee';
-		const fileName = projectData.name + '.json';
-
+		const fileName = projectData.name + '.modulee';
 		downloadJson(projectData, fileName);
 	}
 </script>
