4d17a4580f7380f60d4351087b1464c0177ac1e1
Yolisses <yolisses0@gmail.com>
2025-03-10 15:51:37 -0300

    Make SidebarTabs more responsive

--- src/lib/sidebar/SidebarTabs.svelte
+++ src/lib/sidebar/SidebarTabs.svelte
@@ -2,7 +2,7 @@
 	import Tab from './Tab.svelte';
 </script>
 
-<div class="flex flex-row border-b border-black">
+<div class="flex flex-row overflow-auto border-b border-black">
 	<Tab tab={'internalModule'}>InternalModule</Tab>
 	<Tab tab={'internalModules'}>InternalModules</Tab>
 	<Tab tab={'project'}>Project</Tab>
