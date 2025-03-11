8ac90083bbeae28b5333149c134e530b64aac975
Yolisses <yolisses0@gmail.com>
2025-03-11 07:22:26 -0300

    Use module reference in SetTargetInternalModuleMenu

--- src/lib/internalModule/SetTargetInternalModuleMenu.svelte
+++ src/lib/internalModule/SetTargetInternalModuleMenu.svelte
@@ -3,11 +3,13 @@
 	import { createId } from '$lib/data/createId.js';
 	import { getGraphContext } from '$lib/data/graphContext';
 	import type { InternalModule } from '$lib/data/InternalModule.svelte';
+	import type { InternalModuleData } from '$lib/data/InternalModuleData';
+	import type { InternalModuleReference } from '$lib/data/InternalModuleReference';
+	import type { ModuleReference } from '$lib/data/ModuleReference';
 	import { getEditorContext } from '$lib/editor/editorContext.js';
 	import { getProjectDataContext } from '$lib/project/projectDataContext.js';
 	import BasicList from '$lib/ui/BasicList.svelte';
 	import { getId } from '$lib/ui/getId';
-	import { getName } from '$lib/ui/getName.js';
 	import CreateInternalModuleButton from './CreateInternalModuleButton.svelte';
 
 	interface Props {
@@ -21,19 +23,35 @@
 
 	const { closeModal, moduleNodeId }: Props = $props();
 
-	function handleInternalModuleSelect(internalModule: InternalModule) {
+	function handleSelectModule(moduleReference: ModuleReference) {
 		const addNodeCommand = new SetModuleNodeModuleReferenceCommand({
 			id: createId(),
-			type: 'SetModuleNodeModuleReferenceCommand',
-			details: {
-				moduleNodeId: moduleNodeId,
-				moduleReference: internalModule.id,
-			},
 			createdAt: new Date().toJSON(),
+			details: { moduleNodeId, moduleReference },
+			type: 'SetModuleNodeModuleReferenceCommand',
 			projectId: projectDataContext.projectData.id,
 		});
 		editorContext.editor.execute(addNodeCommand);
 		closeModal();
+	}
+
+	function getInternalModuleReference(
+		internalModuleData: InternalModuleData,
+	): InternalModuleReference {
+		return { id: internalModuleData.id, type: 'internal' };
+	}
+
+	const moduleReferences: ModuleReference[] = $derived(
+		graphContext.graph.internalModules.values().map(getInternalModuleReference),
+	);
+
+	function getName(moduleReference: ModuleReference) {
+		return graphContext.graph.internalModules.get(moduleReference.id).name;
+	}
+
+	function handleInternalModuleCreated(internalModule: InternalModule) {
+		const internalModuleReference = getInternalModuleReference(internalModule);
+		handleSelectModule(internalModuleReference);
 	}
 </script>
 
@@ -41,15 +59,10 @@
 	class="flex max-h-[75vh] flex-col rounded bg-zinc-700 shadow-lg shadow-black/50 outline outline-1 outline-zinc-800"
 >
 	<div class="scroll-small flex flex-col overflow-auto whitespace-nowrap">
-		<BasicList
-			{getId}
-			{getName}
-			onClick={handleInternalModuleSelect}
-			values={graphContext.graph.internalModules.values()}
-		/>
+		<BasicList {getId} {getName} values={moduleReferences} onClick={handleSelectModule} />
 		<CreateInternalModuleButton
 			class="common-button"
-			onInternalModuleCreated={handleInternalModuleSelect}
+			onInternalModuleCreated={handleInternalModuleCreated}
 		/>
 	</div>
 </div>
