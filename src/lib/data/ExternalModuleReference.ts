8b38dd53dff0b6f580a585a590bc765fed177cdb
Yolisses <yolisses0@gmail.com>
2025-03-11 07:29:45 -0300

    Add TODO in module reference types

--- src/lib/data/ExternalModuleReference.ts
+++ src/lib/data/ExternalModuleReference.ts
@@ -1,5 +1,7 @@
 import type { Version } from '$lib/module/Version';
 
+// TODO consider renaming id to moduleId to make clear it's just a reference,
+// not the reference own id
 export type ExternalModuleReference = {
 	id: string;
 	type: 'external';
--- src/lib/data/InternalModuleReference.ts
+++ src/lib/data/InternalModuleReference.ts
@@ -1,3 +1,5 @@
+// TODO consider renaming id to moduleId to make clear it's just a reference,
+// not the reference own id
 export type InternalModuleReference = {
 	id: string;
 	type: 'internal';
