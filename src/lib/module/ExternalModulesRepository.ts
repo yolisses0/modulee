b146247872029167e8db1b2cc030a10e93c22a18
Yolisses <yolisses0@gmail.com>
2025-03-15 08:26:20 -0300

    Add TODO in ExternalModulesRepository.getExternalModules

--- src/lib/module/ExternalModulesRepository.ts
+++ src/lib/module/ExternalModulesRepository.ts
@@ -2,6 +2,7 @@ import type { ExternalModuleData } from './ExternalModuleData';
 
 export interface ExternalModulesRepository {
 	initialize(): Promise<void>;
+	// TODO add external modules ids parameter
 	getExternalModules(): Promise<ExternalModuleData[]>;
 	addExternalModule(externalModuleData: ExternalModuleData): Promise<void>;
 }
