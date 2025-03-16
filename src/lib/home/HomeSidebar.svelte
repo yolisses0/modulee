c7b01f08d485ee79fbe01ddbfae92b8379b89df5
Yolisses <yolisses0@gmail.com>
2025-03-16 12:16:16 -0300

    Use fw in HomeSidebar

--- src/lib/home/HomeSidebar.svelte
+++ src/lib/home/HomeSidebar.svelte
@@ -6,17 +6,17 @@
 <div class="flex flex-col border-r-2 border-black/50">
 	<a class="common-button" href="/" aria-label="Home">
 		<img
-			src="/logo with name.svg"
 			height="16"
+			class="my-1"
 			alt="Modulee logo"
 			style="height: 1rem;"
-			class="my-1"
+			src="/logo with name.svg"
 		/>
 	</a>
 	<a class="common-button" href="/projects">
-		<Fa icon={faProjectDiagram} rotate={180} flip="vertical" /> Projects
+		<Fa fw icon={faProjectDiagram} rotate={180} flip="vertical" /> Projects
 	</a>
 	<a class="common-button" href="/externalModules">
-		<Fa icon={faPuzzlePiece} /> External modules
+		<Fa fw icon={faPuzzlePiece} /> External modules
 	</a>
 </div>
