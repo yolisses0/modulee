5d3f29d6b4ee17420710ffbd33973604510a901c
Yolisses <yolisses0@gmail.com>
2025-03-16 13:21:24 -0300

    Center titles vertically in projects and external modules pages

--- src/lib/module/externalModule/ExternalModulesPage.svelte
+++ src/lib/module/externalModule/ExternalModulesPage.svelte
@@ -10,8 +10,8 @@
 
 <div class="flex flex-col items-center">
 	<div class="flex w-full max-w-xl flex-col gap-4 p-4">
-		<div class="flex flex-row items-center justify-between gap-2">
-			<h1 class="h-10 pl-2 text-xl font-medium">External modules</h1>
+		<div class="flex h-10 flex-row items-center justify-between gap-2">
+			<h1 class="pl-2 text-xl font-medium">External modules</h1>
 		</div>
 		<div>
 			{#await externalModulesDataPromise}
--- src/lib/project/ProjectsPage.svelte
+++ src/lib/project/ProjectsPage.svelte
@@ -15,8 +15,8 @@
 
 <div class="flex flex-col items-center">
 	<div class="flex w-full max-w-xl flex-col gap-4 p-4">
-		<div class="flex flex-row items-center justify-between gap-2">
-			<h1 class="h-10 pl-2 text-xl font-medium">Projects</h1>
+		<div class="flex h-10 flex-row items-center justify-between gap-2">
+			<h1 class="pl-2 text-xl font-medium">Projects</h1>
 			<div class="flex-1"></div>
 			<ImportProjectButton />
 			<CreateProjectButton />
