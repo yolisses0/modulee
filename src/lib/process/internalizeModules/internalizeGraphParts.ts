465ab8d3ab95585d58318df8b38f005fcc5ba82c
Yolisses <yolisses0@gmail.com>
2025-03-16 09:46:41 -0300

    Format internalizeGraphParts

--- src/lib/process/internalizeModules/internalizeGraphParts.ts
+++ src/lib/process/internalizeModules/internalizeGraphParts.ts
@@ -2,15 +2,15 @@ import type { GraphData } from '$lib/data/GraphData';
 import type { GraphRegistry } from '$lib/data/GraphRegistry';
 
 export function internalizeGraphParts(graphRegistry: GraphRegistry, externalGraphData: GraphData) {
-	externalGraphData.internalModules.forEach((internalModuleData) => {
-		graphRegistry.internalModules.add(internalModuleData);
-	});
-
 	externalGraphData.nodes.forEach((nodeData) => {
 		graphRegistry.nodes.add(nodeData);
 	});
 
 	externalGraphData.connections.forEach((connectionData) => {
 		graphRegistry.connections.add(connectionData);
+	});
+
+	externalGraphData.internalModules.forEach((internalModuleData) => {
+		graphRegistry.internalModules.add(internalModuleData);
 	});
 }
