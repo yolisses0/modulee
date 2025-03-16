fe222788a37bce6d99d397765acb7c4fe1777738
Yolisses <yolisses0@gmail.com>
2025-03-16 11:24:46 -0300

    Create HomeSideBar

---
+++ src/lib/home/HomeSidebar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { faProjectDiagram, faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+</script>
+
+<div class="flex flex-col border-r-2 border-black/50">
+	<a class="common-button" href="/" aria-label="Home">
+		<img
+			src="/logo with name.svg"
+			height="16"
+			alt="Modulee logo"
+			style="height: 1rem;"
+			class="my-1"
+		/>
+	</a>
+	<a class="common-button" href="/projects">
+		<Fa icon={faProjectDiagram} rotate={180} flip="vertical" /> Projects
+	</a>
+	<a class="common-button" href="/externalModules">
+		<Fa icon={faPuzzlePiece} /> External modules
+	</a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import HomeSidebar from '$lib/home/HomeSidebar.svelte';
 	import GlobalLayout from '$lib/project/GlobalLayout.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-<GlobalLayout>
-	{@render children()}
-</GlobalLayout>
+<div class="flex min-h-screen flex-row">
+	<HomeSidebar />
+	<div class="flex-1">
+		<GlobalLayout>
+			{@render children()}
+		</GlobalLayout>
+	</div>
+</div>
