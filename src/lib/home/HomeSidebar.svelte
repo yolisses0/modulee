1554a21edc268296bf79b31f125906973bd2b202
Yolisses <yolisses0@gmail.com>
2025-03-25 16:23:45 -0300

    Add lateral border on select tab in HomeSidebar

--- src/lib/home/HomeSidebar.svelte
+++ src/lib/home/HomeSidebar.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import { faProjectDiagram, faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
+
+	const route = $derived(page.route.id || '');
+	$inspect(page);
 </script>
 
 <div class="flex flex-col border-r-2 border-black/50">
@@ -13,10 +17,18 @@
 			src="/logo with name.svg"
 		/>
 	</a>
-	<a class="common-button" href="/projects">
+	<a
+		href="/projects"
+		class="common-button rounded-none border-white/25"
+		class:border-l-4={route.startsWith('/(home)/projects')}
+	>
 		<Fa fw icon={faProjectDiagram} rotate={180} flip="vertical" /> Projects
 	</a>
-	<a class="common-button" href="/externalModules">
+	<a
+		href="/externalModules"
+		class="common-button rounded-none border-white/25"
+		class:border-l-4={route.startsWith('/(home)/externalModules')}
+	>
 		<Fa fw icon={faPuzzlePiece} /> External modules
 	</a>
 </div>
