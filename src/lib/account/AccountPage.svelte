d1d2a4de8d85febc682e5a37d43dbf2279fbe5f0
Yolisses <yolisses0@gmail.com>
2025-03-27 07:00:00 -0300

    Create GoogleSignInButton and use it in AccountPage

--- src/lib/account/AccountPage.svelte
+++ src/lib/account/AccountPage.svelte
@@ -1,1 +1,17 @@
-<div>Account</div>
+<script>
+	import GoogleSignInButton from './GoogleSignInButton.svelte';
+</script>
+
+<div class="flex min-h-[100dvh] flex-col items-center">
+	<div class="flex w-full max-w-xl flex-1 flex-col gap-4 p-4">
+		<div class="flex h-10 flex-row items-center justify-between gap-2">
+			<h1 class="pl-2 text-xl font-medium">Accounts</h1>
+		</div>
+		<div class="flex flex-1 items-center justify-center">
+			<div class="w-full max-w-xs">
+				<GoogleSignInButton />
+			</div>
+		</div>
+		<div class="h-10"></div>
+	</div>
+</div>
---
+++ src/lib/account/GoogleSignInButton.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<script src="https://accounts.google.com/gsi/client" async></script>
+</svelte:head>
+
+<div
+	id="g_id_onload"
+	data-client_id="725523345294-l7ljv04v2maac7k6ugu6ifmuut88gbjk.apps.googleusercontent.com"
+	data-context="signin"
+	data-ux_mode="popup"
+	data-callback="onGoogleSignIn"
+	data-close_on_tap_outside="false"
+	data-itp_support="true"
+></div>
+
+<div
+	class="g_id_signin"
+	data-type="standard"
+	data-shape="rectangular"
+	data-theme="filled_blue"
+	data-text="signin_with"
+	data-size="large"
+	data-logo_alignment="left"
+></div>
