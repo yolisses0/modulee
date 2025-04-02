82f8789f9baca1bca8cdb5229cdda91d19a64126
Yolisses <yolisses0@gmail.com>
2025-04-02 17:02:26 -0300

    Improve UserSchema

--- src/lib/account/UserSchema.ts
+++ src/lib/account/UserSchema.ts
@@ -1,7 +1,8 @@
 import { required } from '$lib/db/required';
 import { Schema } from 'mongoose';
+import type { UserData } from './UserData';
 
-export const UserSchema = new Schema({
+export const UserSchema = new Schema<UserData>({
 	name: required(String),
-	email: required(String),
+	email: { type: String, select: false, required: true, unique: true },
 });
