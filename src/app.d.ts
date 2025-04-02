252fc769f4079a0f75fa84df212e6e6cc491dbd5
Yolisses <yolisses0@gmail.com>
2025-04-02 18:01:54 -0300

    Add session management to hooks and update Locals interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
+import type { Session } from '$lib/session/Session';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			session: Session | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,24 @@
 import { connectToDatabase } from '$lib/db/connectToDatabase';
+import { deleteSessionTokenCookie } from '$lib/session/deleteSessionTokenCookie';
+import { setSessionTokenCookie } from '$lib/session/setSessionTokenCookie';
+import { validateSessionToken } from '$lib/session/validateSessionToken';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	await connectToDatabase();
-	const response = await resolve(event);
-	return response;
+	const token = event.cookies.get('session') ?? null;
+	if (token === null) {
+		event.locals.session = null;
+		return resolve(event);
+	}
+
+	const session = await validateSessionToken(token);
+	if (session !== null) {
+		setSessionTokenCookie(event, token, session.expiresAt);
+	} else {
+		deleteSessionTokenCookie(event);
+	}
+
+	event.locals.session = session;
+	return resolve(event);
 };
