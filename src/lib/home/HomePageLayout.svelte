76322b6978492eed250825caa756dda61621c228
Yolisses <yolisses0@gmail.com>
2025-04-04 16:43:56 -0300

    Refactor HomePageLayout to improve layout structure and enable overflow handling

--- src/lib/home/HomePageLayout.svelte
+++ src/lib/home/HomePageLayout.svelte
@@ -10,14 +10,14 @@
 	const { title, children, topChildren }: Props = $props();
 </script>
 
-<div class="flex flex-col items-center">
-	<div class="flex w-full max-w-xl flex-col gap-4 p-4">
-		<div class="flex h-10 flex-row items-center justify-between gap-2">
-			<h1 class="p-2 text-xl font-medium">{title}</h1>
-			<div class="flex-1"></div>
-			{@render topChildren?.()}
-		</div>
-		<div>
+<div class="flex-1 overflow-hidden">
+	<div class="flex h-[100dvh] flex-col items-center overflow-auto">
+		<div class="flex w-full max-w-xl flex-col gap-4 p-4">
+			<div class="flex h-10 flex-row items-center justify-between gap-2">
+				<h1 class="p-2 text-xl font-medium">{title}</h1>
+				<div class="flex-1"></div>
+				{@render topChildren?.()}
+			</div>
 			{@render children?.()}
 		</div>
 	</div>
