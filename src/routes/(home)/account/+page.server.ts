253418cb9b943e724282949f3d89aff31aa78f1f
Yolisses <yolisses0@gmail.com>
2025-04-04 15:23:56 -0300

    Remove account route

--- src/routes/(home)/account/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from '../externalModules/$types';
-
-export const load: PageServerLoad = async ({ locals }) => {
-	if (!locals.session) {
-		redirect(307, '/signIn');
-	}
-};
--- src/routes/(home)/account/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import AccountPage from '$lib/account/AccountPage.svelte';
-</script>
-
-<AccountPage />
