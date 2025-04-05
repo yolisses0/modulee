b3ed74099b50e603d11e2b52796a1313f61dd9fe
Yolisses <yolisses0@gmail.com>
2025-04-05 07:17:33 -0300

    Replace ImportExternalModuleButton modal by a link

--- src/lib/import/ImportExternalModuleButton.svelte
+++ src/lib/import/ImportExternalModuleButton.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
+	import { getProjectDataContext } from '$lib/project/projectDataContext';
 	import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
-	import ImportExternalModuleModal from './ImportExternalModuleModal.svelte';
 
-	let isModalActive = $state(false);
-
-	function handleClick() {
-		isModalActive = true;
-	}
-
-	function closeModal() {
-		isModalActive = false;
-	}
+	const projectDataContext = getProjectDataContext();
 </script>
 
-<button class="common-button" onclick={handleClick}>
+<a class="common-button" href="/projects/{projectDataContext.projectData.id}/externalModules">
 	<Fa icon={faArrowCircleDown} />
 	Import external module
-</button>
-
-{#if isModalActive}
-	<ImportExternalModuleModal {closeModal} />
-{/if}
+</a>
