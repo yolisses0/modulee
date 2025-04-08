f61aa60cfa09d718d7c4039d167cc0b9a6b84d23
Yolisses <yolisses0@gmail.com>
2025-04-08 17:04:01 -0300

    Add description in ProjectScheme and update characters limit

--- src/lib/db/ProjectSchema.ts
+++ src/lib/db/ProjectSchema.ts
@@ -2,7 +2,8 @@ import { Schema } from 'mongoose';
 
 export const ProjectSchema = new Schema(
 	{
-		name: { type: String, required: true, maxlength: 255 },
+		name: { type: String, required: true, maxlength: 100 },
+		description: { type: String, required: false, maxlength: 1000 },
 	},
 	{
 		timestamps: true,
