e91c468fdb818b9c34af6cdd255f5baa160bccfb
Yolisses <yolisses0@gmail.com>
2025-04-08 17:15:08 -0300

    Refactor SidebarV2 to include FontAwesome icons for navigation links

--- src/lib/sidebar/SidebarV2.svelte
+++ src/lib/sidebar/SidebarV2.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { getInternalModuleIdContext } from '$lib/module/internalModule/internalModuleIdContext';
 	import { getProjectDataContext } from '$lib/project/projectDataContext';
+	import { faFileAlt, faProjectDiagram, faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
 
 	const projectDataContext = getProjectDataContext();
 	const baseUrl = $derived('/projects/' + projectDataContext.projectData.id);
@@ -12,10 +14,17 @@
 		class="common-button"
 		href="{baseUrl}/internalModules/{internalModuleIdContext.internalModuleId}"
 	>
+		<Fa icon={faProjectDiagram} flip="horizontal" />
 		Nodes
 	</a>
-	<a class="common-button" href="{baseUrl}/modules"> Modules </a>
-	<a class="common-button" href={baseUrl}> Project </a>
+	<a class="common-button" href="{baseUrl}/modules">
+		<Fa icon={faPuzzlePiece} />
+		Modules
+	</a>
+	<a class="common-button" href={baseUrl}>
+		<Fa icon={faFileAlt} />
+		Project
+	</a>
 	<div class="flex-1"></div>
 	<a class="common-button" href="/" aria-label="Home">
 		<img
