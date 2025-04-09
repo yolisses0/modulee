06593db0c7c434897007cd66816d5d7fae13e51f
Yolisses <yolisses0@gmail.com>
2025-04-09 19:05:35 -0300

    Remove (home)/externalModules route

--- src/routes/(home)/externalModules/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import ExternalModulesPage from '$lib/module/externalModule/ExternalModulesPage.svelte';
-	import type { PageProps } from './$types';
-
-	const { data }: PageProps = $props();
-</script>
-
-<ExternalModulesPage externalModulesData={data.externalModulesData} />
