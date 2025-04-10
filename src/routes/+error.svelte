ce11e611239df99ff6d1502a5647e78c0533c787
Yolisses <yolisses0@gmail.com>
2025-04-10 16:30:43 -0300

    Update error page to navigate back to the previous page or home

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { onMount } from 'svelte';
+
+	let href = '/';
+
+	onMount(() => {
+		// Send the user to last page in the app or to home page.
+		const url = new URL(document.referrer);
+		if (url.origin === location.origin) {
+			href = document.referrer;
+		} else {
+			href = '/';
+		}
+	});
 </script>
 
-<div class="flex min-h-screen flex-1 flex-col items-center justify-center gap-2 p-8">
-	<div class="flex flex-1 flex-col items-center justify-end gap-2">
-		<h1 class="text-xl">Sorry, we could not open this page.</h1>
-		<a href="/" class="primary-button">Go home</a>
-	</div>
-	<div class="flex flex-1 flex-col items-center justify-center gap-2">
-		<div>{page.status}</div>
-		<div>{page.error?.message}</div>
-	</div>
+<div class="flex min-h-screen flex-1 flex-col items-center justify-center gap-4 p-8">
+	<div class="text-xl">{page.error?.message}</div>
+	<div>{page.status}</div>
+	<a {href} class="primary-button">Go back</a>
 </div>
