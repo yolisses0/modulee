9323e096ca7a3b8432bc2cbb75b5baa2e9b39548
Yolisses <yolisses0@gmail.com>
2025-04-10 16:21:05 -0300

    Add error handling page to display status and error messages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="flex min-h-screen flex-1 flex-col items-center justify-center gap-2 p-8">
+	<div class="flex flex-1 flex-col items-center justify-end gap-2">
+		<h1 class="text-xl">Sorry, we could not open this page.</h1>
+		<a href="/" class="primary-button">Go home</a>
+	</div>
+	<div class="flex flex-1 flex-col items-center justify-center gap-2">
+		<div>{page.status}</div>
+		<div>{page.error?.message}</div>
+	</div>
+</div>
