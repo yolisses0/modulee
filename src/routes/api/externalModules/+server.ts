95a915e45b52768e1f54fcea36586632e5c488a1
Yolisses <yolisses0@gmail.com>
2025-04-12 09:02:56 -0300

    Add GET handler to fetch external modules data

--- src/routes/api/externalModules/+server.ts
+++ src/routes/api/externalModules/+server.ts
@@ -1,6 +1,12 @@
 import { ExternalModuleModel } from '$lib/db/externalModule/ExternalModuleModel';
+import { getExternalModulesData } from '$lib/db/externalModule/getExternalModulesData';
 import { getSession } from '$lib/user/getSession';
 import { json, type RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+	const externalModulesData = await getExternalModulesData();
+	return json(externalModulesData);
+};
 
 export const POST: RequestHandler = async ({ request, locals }) => {
 	const { userId } = getSession(locals);
