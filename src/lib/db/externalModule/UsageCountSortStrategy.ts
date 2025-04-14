e8b6d87f0cb8627eda3716fb75ccb365543a911e
Yolisses <yolisses0@gmail.com>
2025-04-14 07:37:08 -0300

    Fix UsageCountSortStrategy

--- src/lib/db/externalModule/UsageCountSortStrategy.ts
+++ src/lib/db/externalModule/UsageCountSortStrategy.ts
@@ -1,19 +1,22 @@
 import type { ExternalModuleData } from '$lib/module/externalModule/ExternalModuleData';
+import { Types } from 'mongoose';
 import { PaginationStrategy } from './PaginationStrategy';
 
 type CursorData = { _id: string; usageCount: number };
 
 export class UsageCountSortStrategy extends PaginationStrategy {
 	getSortStage() {
-		return { usageCount: -1, _id: -1 } as const;
+		return { $sort: { usageCount: -1, _id: -1 } } as const;
 	}
 
 	getFilterStage(cursorData: CursorData) {
 		return {
-			$or: [
-				{ usageCount: { $lt: cursorData.usageCount } },
-				{ usageCount: cursorData.usageCount, _id: { $lte: cursorData._id } },
-			],
+			$match: {
+				$or: [
+					{ usageCount: { $lt: cursorData.usageCount } },
+					{ usageCount: cursorData.usageCount, _id: { $lte: new Types.ObjectId(cursorData._id) } },
+				],
+			},
 		};
 	}
 
