d6e064f8a4df4738eb294c884f8a41a0bb76e2a2
Yolisses <yolisses0@gmail.com>
2025-04-16 07:05:21 -0300

    Rename REDIS_DB_URL to REDISDB_URI for consistency

--- src/lib/session/getRedisClient.ts
+++ src/lib/session/getRedisClient.ts
@@ -1,6 +1,6 @@
-import { REDIS_DB_URL } from '$env/static/private';
+import { REDISDB_URI } from '$env/static/private';
 import { createClient } from 'redis';
 
 export async function getRedisClient() {
-	return await createClient({ url: REDIS_DB_URL }).connect();
+	return await createClient({ url: REDISDB_URI }).connect();
 }
