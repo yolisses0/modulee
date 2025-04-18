07d12d107a37d9f4324eae278855fc065400c4a5
Yolisses <yolisses0@gmail.com>
2025-04-17 22:09:36 -0300

    Add copy data context to Contexts

--- src/lib/shortcut/Contexts.svelte.ts
+++ src/lib/shortcut/Contexts.svelte.ts
@@ -1,3 +1,4 @@
+import { getCopyDataContext } from '$lib/copy/copyDataContext';
 import { getGraphContext } from '$lib/data/graphContext';
 import { getEditorContext } from '$lib/editor/editorContext';
 import { getAudioBackendContext } from '$lib/engine/audioBackendContext';
@@ -9,12 +10,15 @@ import { getZoomContext } from '$lib/space/zoom/zoomContext';
 import { getSelectedNodeIdsContext } from 'nodes-editor';
 import { defaultShortcuts } from './defaultShortcuts';
 
+// TODO consider using getContext without possible throwing to make shortcuts
+// not so dependent of page setup
 export class Contexts {
 	shortcuts = defaultShortcuts;
 	zoomContext = getZoomContext();
 	graphContext = getGraphContext();
 	editorContext = getEditorContext();
 	isMutedContext = getIsMutedContext();
+	copyDataContext = getCopyDataContext();
 	projectDataContext = getProjectDataContext();
 	audioBackendContext = getAudioBackendContext();
 	graphRegistryContext = getGraphRegistryContext();
