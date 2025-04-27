6821cb8150a34ad93404d964e63715fd1957adfc
Yolisses <yolisses0@gmail.com>
2025-04-27 08:40:31 -0300

    Hide InternalModulesNavbar if there's a single internal module

--- src/lib/node/InternalModulesNavbar.svelte
+++ src/lib/node/InternalModulesNavbar.svelte
@@ -6,17 +6,22 @@
 	const graphContext = getGraphContext();
 	const projectDataContext = getProjectDataContext();
 	const internalModuleIdContext = getInternalModuleIdContext();
-	const projectId = projectDataContext.projectData.id;
+
+	const projectId = $derived(projectDataContext.projectData.id);
+	const internalModules = $derived(graphContext.graph.internalModules.values());
 </script>
 
-<div class="flex flex-row overflow-auto border-b-2 border-black/50">
-	{#each graphContext.graph.internalModules.values() as internalModule (internalModule.id)}
-		<a
-			href="/projects/{projectId}/internalModules/{internalModule.id}/graph"
-			class="horizontal-tab block max-w-[10rem] overflow-hidden text-ellipsis whitespace-nowrap"
-			class:horizontal-tab-selected={internalModuleIdContext.internalModuleId === internalModule.id}
-		>
-			{internalModule.name}
-		</a>
-	{/each}
-</div>
+{#if internalModules.length > 1}
+	<div class="flex flex-row overflow-auto border-b-2 border-black/50">
+		{#each graphContext.graph.internalModules.values() as internalModule (internalModule.id)}
+			<a
+				href="/projects/{projectId}/internalModules/{internalModule.id}/graph"
+				class="horizontal-tab block max-w-[10rem] overflow-hidden text-ellipsis whitespace-nowrap"
+				class:horizontal-tab-selected={internalModuleIdContext.internalModuleId ===
+					internalModule.id}
+			>
+				{internalModule.name}
+			</a>
+		{/each}
+	</div>
+{/if}
