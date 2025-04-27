a268a41dc1dc133c748f6b44b7ea7c5b5a81fd7c
Yolisses <yolisses0@gmail.com>
2025-04-27 07:26:18 -0300

    Separate RackModuleItem

---
+++ src/lib/rack/RackModuleItem.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { InternalModule } from '$lib/data/InternalModule.svelte';
+	import { faChevronDown, faPlus } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+	import { getIsSomeModuleNode } from './getIsSomeModuleNode';
+	import RackPanel from './RackPanel.svelte';
+
+	interface Props {
+		internalModule: InternalModule;
+	}
+
+	const { internalModule }: Props = $props();
+</script>
+
+<div class="flex flex-col items-center">
+	<div class="flex w-full max-w-xl flex-row gap-2 self-center">
+		<button class="common-button">
+			<Fa icon={faChevronDown} fw />
+		</button>
+		<h2 class="py-2 text-lg">{internalModule.name}</h2>
+		<div class="flex-1"></div>
+		<button class="common-button">
+			<Fa icon={faPlus} />
+			Add effect
+		</button>
+	</div>
+	<div class="flex flex-row flex-wrap justify-center gap-1">
+		{#each internalModule.nodes.filter(getIsSomeModuleNode) as moduleNode}
+			<RackPanel {moduleNode} />
+		{/each}
+	</div>
+</div>
--- src/lib/rack/RackPage.svelte
+++ src/lib/rack/RackPage.svelte
@@ -1,32 +1,12 @@
 <script lang="ts">
 	import { getGraphContext } from '$lib/data/graphContext';
-	import { faChevronDown, faPlus } from '@fortawesome/free-solid-svg-icons';
-	import Fa from 'svelte-fa';
-	import { getIsSomeModuleNode } from './getIsSomeModuleNode';
-	import RackPanel from './RackPanel.svelte';
+	import RackModuleItem from './RackModuleItem.svelte';
 
 	const graphContext = getGraphContext();
 </script>
 
 <div class="flex flex-col gap-4 p-4">
 	{#each graphContext.graph.internalModules.values() as internalModule (internalModule.id)}
-		<div class="flex flex-col items-center">
-			<div class="flex w-full max-w-xl flex-row gap-2 self-center">
-				<button class="common-button">
-					<Fa icon={faChevronDown} fw />
-				</button>
-				<h2 class="py-2 text-lg">{internalModule.name}</h2>
-				<div class="flex-1"></div>
-				<button class="common-button">
-					<Fa icon={faPlus} />
-					Add effect
-				</button>
-			</div>
-			<div class="flex flex-row flex-wrap justify-center gap-1">
-				{#each internalModule.nodes.filter(getIsSomeModuleNode) as moduleNode}
-					<RackPanel {moduleNode} />
-				{/each}
-			</div>
-		</div>
+		<RackModuleItem {internalModule} />
 	{/each}
 </div>
