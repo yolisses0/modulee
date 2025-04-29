d21df2c96b5a129c046854d17b92aaadf3c90918
Yolisses <yolisses0@gmail.com>
2025-04-29 20:05:19 -0300

    Add TODO pass velocity data too in setNoteOn

--- src/lib/engine/AudioBackend.ts
+++ src/lib/engine/AudioBackend.ts
@@ -2,6 +2,7 @@ import type { GraphEngineData } from './data/GraphEngineData';
 
 export interface AudioBackend {
 	destroy(): void;
+	// TODO pass velocity data too
 	setNoteOn(pitch: number): void;
 	setNoteOff(pitch: number): void;
 	setIsMuted(isMuted: boolean): void;
