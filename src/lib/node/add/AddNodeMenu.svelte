1aaa6f305f4d607be54047b97acde244205393f9
Yolisses <yolisses0@gmail.com>
2025-05-01 15:53:09 -0300

    Separate AddNodeMenuSearchInput from AddNodeMenu

--- src/lib/node/add/AddNodeMenu.svelte
+++ src/lib/node/add/AddNodeMenu.svelte
@@ -2,6 +2,7 @@
 	import type { Vector } from 'nodes-editor';
 	import AddNodeCategoryItems from './AddNodeCategoryItems.svelte';
 	import { AddNodeMenuLogic } from './AddNodeMenuLogic.svelte';
+	import AddNodeMenuSearchInput from './AddNodeMenuSearchInput.svelte';
 	import AddNodeSearchNodeItems from './AddNodeSearchNodeItems.svelte';
 
 	interface Props {
@@ -10,31 +11,14 @@
 	}
 
 	const { closeModal, screenPosition }: Props = $props();
-
 	const addNodeMenuLogic = new AddNodeMenuLogic(closeModal, screenPosition);
-
-	function handleKeyDown(e: KeyboardEvent) {
-		if (e.key !== 'Enter') return;
-		if (addNodeMenuLogic.searchText.length === 0) return;
-		const option = addNodeMenuLogic.getOptions()[0];
-		if (!option) return;
-		addNodeMenuLogic.handleNodeTypeSelect(option);
-	}
 </script>
 
 <!-- TODO consider adding a descriptive text like "Add node" -->
 <div
 	class="flex max-h-[75vh] flex-col rounded bg-zinc-700 shadow-lg shadow-black/50 outline-1 outline-zinc-800"
 >
-	<!-- svelte-ignore a11y_autofocus -->
-	<input
-		autofocus
-		type="search"
-		placeholder="Search"
-		class="common-input"
-		onkeydown={handleKeyDown}
-		bind:value={addNodeMenuLogic.searchText}
-	/>
+	<AddNodeMenuSearchInput {addNodeMenuLogic} />
 	<div class="scroll-small flex flex-col overflow-auto overscroll-contain whitespace-nowrap">
 		{#if addNodeMenuLogic.searchText}
 			<AddNodeSearchNodeItems {addNodeMenuLogic} />
---
+++ src/lib/node/add/AddNodeMenuSearchInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { AddNodeMenuLogic } from './AddNodeMenuLogic.svelte';
+
+	interface Props {
+		addNodeMenuLogic: AddNodeMenuLogic;
+	}
+
+	const { addNodeMenuLogic }: Props = $props();
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.key !== 'Enter') return;
+		if (addNodeMenuLogic.searchText.length === 0) return;
+		const option = addNodeMenuLogic.getOptions()[0];
+		if (!option) return;
+		addNodeMenuLogic.handleNodeTypeSelect(option);
+	}
+</script>
+
+<!-- svelte-ignore a11y_autofocus -->
+<input
+	autofocus
+	type="search"
+	placeholder="Search"
+	class="common-input"
+	onkeydown={handleKeyDown}
+	bind:value={addNodeMenuLogic.searchText}
+/>
