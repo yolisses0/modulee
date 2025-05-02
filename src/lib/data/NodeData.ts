8fe1e86e13778cb43f3ee7d086055546c289453c
Yolisses <yolisses0@gmail.com>
2025-05-02 07:45:00 -0300

    Add comment in NodeData

--- src/lib/data/NodeData.ts
+++ src/lib/data/NodeData.ts
@@ -3,4 +3,6 @@ import type { InputNodeData } from './variants/InputNodeData';
 import type { ModuleNodeData } from './variants/ModuleNodeData';
 import type { ModuleVoicesNodeData } from './variants/ModuleVoicesNodeData';
 
+// Technically, a NodeData can be a NodeDataBase<unknown, unknown>, but it
+// doesn't seem necessary and could lead to reduced IntelliSense.
 export type NodeData = InputNodeData | ModuleNodeData | ConstantNodeData | ModuleVoicesNodeData;
