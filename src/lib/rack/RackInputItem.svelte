c9acbf40ea0d9f2afa6bb8746938ec474a06d39c
Yolisses <yolisses0@gmail.com>
2025-05-17 16:24:00 -0300

    Use audio backend updateControl in rack input item

--- src/lib/rack/RackInputItem.svelte
+++ src/lib/rack/RackInputItem.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
 	import type { Input } from '$lib/data/Input.svelte';
+	import { getAudioBackendContext } from '$lib/engine/audioBackendContext';
+	import { hashToUsize } from '$lib/engine/data/hashToUsize';
 
 	interface Props {
 		input: Input;
 	}
 
 	const { input }: Props = $props();
-	let value = $state(0.2);
-	const max = 1;
-	const min = 0;
+	let value = $state(input.getUnconnectedValue());
+	const { min, max, isBoolean } = input.getInputDefinition();
+
+	const audioBackendContext = getAudioBackendContext();
+
+	function handleKeyPress(e: Event) {
+		e.preventDefault();
+	}
+
+	function handleInput() {
+		const id = hashToUsize(input.getControlNodeId());
+		audioBackendContext.audioBackend?.updateControl(id, value);
+	}
 </script>
 
 <div>{input.name}</div>
-<input type="range" bind:value {max} {min} step="any" />
+<input
+	{max}
+	{min}
+	bind:value
+	type="range"
+	oninput={handleInput}
+	onkeypress={handleKeyPress}
+	step={isBoolean ? 1 : 'any'}
+/>
 <input type="text" bind:value class="text-center" />
