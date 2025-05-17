b54f666918fb8cc76d857284cde0831f9f74099f
Yolisses <yolisses0@gmail.com>
2025-05-17 19:01:37 -0300

    Style active like button

--- src/lib/module/externalModule/LikeButton.svelte
+++ src/lib/module/externalModule/LikeButton.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import { faHeart } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
+
+	let isActive = $state(false);
+
+	function handleCLick() {
+		isActive = !isActive;
+	}
 </script>
 
-<button class="common-button">
-	<Fa fw icon={faHeart} />
-	Like
+<button class="common-button" onclick={handleCLick}>
+	<Fa fw icon={faHeart} color={isActive ? 'oklch(63.7% 0.237 25.331)' : undefined} />
+	{isActive ? 'Liked' : 'Like'}
 </button>
