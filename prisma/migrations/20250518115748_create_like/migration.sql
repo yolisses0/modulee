11a1897b60d25bf70b89084ba719c858508af513
Yolisses <yolisses0@gmail.com>
2025-05-18 08:58:08 -0300

    Create like table

---
+++ prisma/migrations/20250518115748_create_like/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "externalModuleId" TEXT NOT NULL,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_externalModuleId_fkey" FOREIGN KEY ("externalModuleId") REFERENCES "ExternalModule"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
