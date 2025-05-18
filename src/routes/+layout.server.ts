c1fdd3c32a9d2c22102d5385e5c82d8645d1d1bd
Yolisses <yolisses0@gmail.com>
2025-05-18 09:56:08 -0300

    Fix user data loading

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,11 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const { userId } = locals.session || {};
-	const userData = await prisma.user.findUnique({ where: { id: userId } });
-	return { userData };
+
+	if (userId) {
+		const userData = await prisma.user.findUnique({ where: { id: userId } });
+		return { userData };
+	} else {
+		return { userData: null };
+	}
 };
