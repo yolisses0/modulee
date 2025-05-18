72113fe156408ebaa7abb1dbceba509bcef96227
Yolisses <yolisses0@gmail.com>
2025-05-18 19:27:14 -0300

    Use DotsMenuButton in InternalModulesNavbar

--- src/lib/node/InternalModulesNavbar.svelte
+++ src/lib/node/InternalModulesNavbar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { getGraphContext } from '$lib/data/graphContext';
 	import { getInternalModuleIdContext } from '$lib/module/internalModule/internalModuleIdContext';
+	import DotsMenuButton from '$lib/project/DotsMenuButton.svelte';
 	import { getProjectDataContext } from '$lib/project/projectDataContext';
 
 	const graphContext = getGraphContext();
@@ -14,15 +15,27 @@
 {#if internalModules.length > 1}
 	<div class="flex flex-row overflow-auto border-b-2 border-black/50">
 		{#each graphContext.graph.internalModules.values() as internalModule (internalModule.id)}
-			<a
-				title={internalModule.name}
-				href="/projects/{projectId}/internalModules/{internalModule.id}/graph"
-				class="horizontal-tab block max-w-[10rem] overflow-hidden text-ellipsis whitespace-nowrap"
+			<div
+				class="horizontal-tab group flex max-w-[10rem] flex-row p-0"
 				class:horizontal-tab-selected={internalModuleIdContext.internalModuleId ===
 					internalModule.id}
 			>
-				{internalModule.name}
-			</a>
+				<a
+					title={internalModule.name}
+					href="/projects/{projectId}/internalModules/{internalModule.id}/graph"
+					class="block overflow-hidden p-2 text-ellipsis whitespace-nowrap"
+				>
+					{internalModule.name}
+				</a>
+				{#if internalModuleIdContext.internalModuleId === internalModule.id}
+					<DotsMenuButton>
+						<div>DEBUG</div>
+						<!-- <RenameProjectButton projectData={item} />
+						<DeleteProjectButton projectId={item.id} />
+						<DownloadProjectButton projectData={item} /> -->
+					</DotsMenuButton>
+				{/if}
+			</div>
 		{/each}
 	</div>
 {/if}
