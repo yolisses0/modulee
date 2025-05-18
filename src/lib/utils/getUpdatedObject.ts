6f3b58ac32267a032b6f658ab1d3f9f0a3c986bf
Yolisses <yolisses0@gmail.com>
2025-05-18 18:28:05 -0300

    Refactor getUpdatedObject to use Object.keys and improve type safety

--- src/lib/utils/getUpdatedObject.ts
+++ src/lib/utils/getUpdatedObject.ts
@@ -1,7 +1,8 @@
 export function getUpdatedObject<T extends object>(old: T, partial: Partial<T>) {
-	Object.entries(old).forEach(([key]) => {
-		if (partial[key] !== undefined) {
-			old[key] = partial[key];
+	Object.keys(old).forEach((key) => {
+		const typedKey = key as keyof T;
+		if (partial[typedKey] !== undefined) {
+			old[typedKey] = partial[typedKey] as T[keyof T];
 		}
 	});
 }
