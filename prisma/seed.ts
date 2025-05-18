9ba5c79ea59552368166b0d0b3d0305ea273f760
Yolisses <yolisses0@gmail.com>
2025-05-18 08:40:05 -0300

    Add one external module in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,19 +5,32 @@ const prisma = new PrismaClient();
 const userData: Prisma.UserCreateInput[] = [
 	{
 		name: 'Alice',
-		email: 'alice@prisma.io',
-		username: 'alice',
 		isSeeded: true,
+		username: 'alice',
+		email: 'alice@email.com',
 	},
 	{
 		name: 'Bob',
-		email: 'bob@prisma.io',
-		username: 'bob',
 		isSeeded: true,
+		username: 'bob',
+		email: 'bob@email.com',
+		externalModules: {
+			create: [
+				{
+					name: 'test1',
+					isSeeded: true,
+					projectId: 'test1',
+					description: 'test1',
+				},
+			],
+		},
 	},
 ];
 
 export async function main() {
+	await prisma.externalModule.deleteMany({ where: { isSeeded: true } });
+	await prisma.user.deleteMany({ where: { isSeeded: true } });
+
 	for (const u of userData) {
 		await prisma.user.create({ data: u });
 	}
