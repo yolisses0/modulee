d8b47546da621db93b80e1a417c89411753db0ff
Yolisses <yolisses0@gmail.com>
2025-05-18 11:29:19 -0300

    Delete UserDocument

--- src/lib/user/UserDocument.ts
+++
@@ -1,5 +0,0 @@
-import type { Has_Id } from '$lib/db/externalModule/Has_Id';
-import type { HasIsSeeded } from '$lib/db/externalModule/HasIsSeeded';
-import type { UserData } from './UserData';
-
-export type UserDocument = UserData & Has_Id & HasIsSeeded;
