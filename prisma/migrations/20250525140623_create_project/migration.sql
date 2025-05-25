c799eb04ad2bac16f3ba9362ac9aad0f598b7849
Yolisses <yolisses0@gmail.com>
2025-05-25 11:08:08 -0300

    Create Project model

---
+++ prisma/migrations/20250525140623_create_project/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "graph" JSONB NOT NULL,
+    "description" VARCHAR(1000),
+    "moduleType" "ModuleType" NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
