2aa87075fee2459033cec8d1776eda835f039ece
Yolisses <yolisses0@gmail.com>
2025-05-25 10:45:44 -0300

    Remove project repository loading in ProjectsPage

--- src/lib/project/ProjectsPage.svelte
+++ src/lib/project/ProjectsPage.svelte
@@ -1,36 +1,21 @@
 <script lang="ts">
 	import ListPageLayout from '$lib/ui/ListPageLayout.svelte';
-	import Spinner from '$lib/ui/Spinner.svelte';
 	import CreateProjectButton from './CreateProjectButton.svelte';
-	import { getProjectsRepository } from './getProjectsRepository';
+	import type { ProjectData } from './ProjectData';
 	import ProjectList from './ProjectList.svelte';
-	import ImportProjectButton from './upload/UploadProjectButton.svelte';
 
-	const projectsRepository = getProjectsRepository();
+	interface Props {
+		projectsData: ProjectData[];
+	}
 
-	let projectsDataPromise = $state(projectsRepository.getProjects());
-	projectsRepository.onProjectsChange = () => {
-		projectsDataPromise = projectsRepository.getProjects();
-	};
+	const { projectsData }: Props = $props();
 </script>
 
 <ListPageLayout title="Projects">
 	{#snippet topChildren()}
-		<ImportProjectButton />
 		<CreateProjectButton />
 	{/snippet}
 	{#snippet children()}
-		{#await projectsDataPromise}
-			<div class="flex h-full flex-1 flex-col items-center p-8">
-				<Spinner></Spinner>
-			</div>
-		{:then projectsData}
-			<ProjectList {projectsData} />
-		{:catch error}
-			<div class="text-red-500">
-				<div>It was not possible to load the projects</div>
-				<div>{error}</div>
-			</div>
-		{/await}
+		<ProjectList {projectsData} />
 	{/snippet}
 </ListPageLayout>
