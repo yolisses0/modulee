61bc93c7aef001df11d53de213374ff76437732f
Yolisses <yolisses0@gmail.com>
2025-05-25 10:40:19 -0300

    Delete +page.ts used to initialize project repository

--- src/routes/(home)/projects/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getProjectsRepository } from '$lib/project/getProjectsRepository';
-import type { PageLoad } from './$types';
-
-export const ssr = false;
-
-export const load: PageLoad = async () => {
-	const projectsRepository = getProjectsRepository();
-	await projectsRepository.initialize();
-};
