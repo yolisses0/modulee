1797da22361431912375b12dc4e68dc159bef583
Yolisses <yolisses0@gmail.com>
2025-05-29 07:51:15 -0300

    Create OutputNodeData

--- src/lib/data/NodeData.ts
+++ src/lib/data/NodeData.ts
@@ -3,11 +3,13 @@ import type { ControlNodeData } from './variants/ControlNodeData';
 import type { InputNodeData } from './variants/InputNodeData';
 import type { ModuleNodeData } from './variants/ModuleNodeData';
 import type { ModuleVoicesNodeData } from './variants/ModuleVoicesNodeData';
+import type { OutputNodeData } from './variants/OutputNodeData';
 
-// Technically, a NodeData can be a NodeDataBase<unknown, unknown>, but it
-// doesn't seem necessary and could lead to reduced IntelliSense.
+// Technically, a NodeData can represent any node variant, but it doesn't seem
+// necessary.
 export type NodeData =
 	| InputNodeData
+	| OutputNodeData
 	| ModuleNodeData
 	| ControlNodeData
 	| ConstantNodeData
