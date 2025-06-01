03b13f7d16866c84acb52ed053dfc8221a68e64a
Yolisses <yolisses0@gmail.com>
2025-06-01 11:21:43 -0300

    Make setSessionTokenCookie more secure

--- src/lib/session/setSessionTokenCookie.ts
+++ src/lib/session/setSessionTokenCookie.ts
@@ -1,11 +1,14 @@
+import { env } from '$env/dynamic/private';
 import type { Cookies } from '@sveltejs/kit';
 import { SESSION_COOKIE_NAME } from './SESSION_COOKIE_NAME';
 
 export function setSessionTokenCookie(cookies: Cookies, token: string, expiresAt: Date): void {
 	cookies.set(SESSION_COOKIE_NAME, token, {
+		path: '/',
 		httpOnly: true,
 		sameSite: 'lax',
+		priority: 'high',
 		expires: expiresAt,
-		path: '/',
+		secure: env.NODE_ENV !== 'development',
 	});
 }
