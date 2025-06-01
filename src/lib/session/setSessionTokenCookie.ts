ebc5438b44436943e6f399f5939fa049105fd76c
Yolisses <yolisses0@gmail.com>
2025-06-01 14:48:59 -0300

    Set httpOnly false in session cookie

--- src/lib/session/setSessionTokenCookie.ts
+++ src/lib/session/setSessionTokenCookie.ts
@@ -3,10 +3,11 @@ import type { Cookies } from '@sveltejs/kit';
 import { SESSION_COOKIE_NAME } from './SESSION_COOKIE_NAME';
 
 export function setSessionTokenCookie(cookies: Cookies, token: string, expiresAt: Date): void {
+	// TODO find a way to set httpOnly true when not using JUCE.
 	cookies.set(SESSION_COOKIE_NAME, token, {
 		path: '/',
-		httpOnly: true,
 		sameSite: 'lax',
+		httpOnly: false,
 		priority: 'high',
 		expires: expiresAt,
 		secure: env.NODE_ENV !== 'development',
