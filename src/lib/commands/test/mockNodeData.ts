b07f74b72d09c467529397deb55d805518203bae
Yolisses <yolisses0@gmail.com>
2025-06-01 11:08:57 -0300

    Fix import in mockCommandData

--- src/lib/commands/test/mockNodeData.ts
+++ src/lib/commands/test/mockNodeData.ts
@@ -1,4 +1,4 @@
-import { EditorCommandData } from '../../editor/EditorCommandData';
+import type { EditorCommandData } from '$lib/editor/EditorCommandData';
 
 export function mockCommandData<T>(details: T): EditorCommandData<T> {
 	return { details } as EditorCommandData<T>;
