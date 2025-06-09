f6ca888951a9e8cb6f6fcf18f7c3c456a5d92843
Yolisses <yolisses0@gmail.com>
2025-06-09 17:31:08 -0300

    Create HomeLayout

---
+++ src/lib/home/HomeLayout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import HomeNavbar from '$lib/home/HomeNavbar.svelte';
+	import { getBaseRouteContext } from '$lib/ui/baseRouteContext';
+	let { children } = $props();
+
+	const baseRouteContext = getBaseRouteContext();
+	baseRouteContext.baseRoute = '';
+</script>
+
+<div class="flex max-h-screen flex-row overflow-hidden">
+	<HomeNavbar />
+	<div class="flex-1">
+		{@render children()}
+	</div>
+</div>
--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import HomeNavbar from '$lib/home/HomeNavbar.svelte';
+	import HomeLayout from '$lib/home/HomeLayout.svelte';
 	let { children } = $props();
 </script>
 
-<div class="flex max-h-screen flex-row overflow-hidden">
-	<HomeNavbar />
-	<div class="flex-1">
-		{@render children()}
-	</div>
-</div>
+<HomeLayout>
+	{@render children()}
+</HomeLayout>
