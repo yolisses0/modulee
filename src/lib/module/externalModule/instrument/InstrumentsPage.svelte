546f1b5684b097de71919183d72ac1ec83c69832
Yolisses <yolisses0@gmail.com>
2025-06-11 07:46:28 -0300

    Create UseInstrumentButton

--- src/lib/module/externalModule/instrument/InstrumentsPage.svelte
+++ src/lib/module/externalModule/instrument/InstrumentsPage.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+	import type { ExternalModuleData } from '../ExternalModuleData';
 	import ExternalModulesPage from '../ExternalModulesPage.svelte';
+	import UseInstrumentButton from './UseInstrumentButton.svelte';
 </script>
 
-<ExternalModulesPage title="Instruments" moduleType="instrument" />
+{#snippet buttons(externalModuleData: ExternalModuleData)}
+	<UseInstrumentButton {externalModuleData} />
+{/snippet}
+
+<ExternalModulesPage title="Instruments" moduleType="instrument" {buttons} />
---
+++ src/lib/module/externalModule/instrument/UseInstrumentButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { faDownload } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+	import type { ExternalModuleData } from '../ExternalModuleData';
+
+	interface Props {
+		externalModuleData: ExternalModuleData;
+	}
+
+	const { externalModuleData }: Props = $props();
+
+	function handleClick() {
+		// TODO
+	}
+</script>
+
+<button class="common-button" onclick={handleClick}>
+	<Fa fw icon={faDownload} />
+	Use
+</button>
