d265ddafd2d6a637b16cd9bc564e0f64b8921d46
Yolisses <yolisses0@gmail.com>
2025-06-13 21:18:04 -0300

    Set closePath in SearchExternalModuleButton

--- src/lib/module/internalModule/SearchExternalModuleButton.svelte
+++ src/lib/module/internalModule/SearchExternalModuleButton.svelte
@@ -1,12 +1,15 @@
 <script>
+	import { page } from '$app/state';
 	import { getProjectDataContext } from '$lib/project/projectDataContext';
+	import { getBaseRouteContext } from '$lib/ui/baseRouteContext';
 	import { faSearch } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
+	const baseRouteContext = getBaseRouteContext();
 	const projectDataContext = getProjectDataContext();
 </script>
 
-<a href="/projects/{projectDataContext.projectData.id}/externalModules" class="common-button">
+<a class="common-button" href="{baseRouteContext.baseRoute}/externalModules?closePath={page.url}">
 	<Fa fw icon={faSearch} />
 	Search for external module
 </a>
