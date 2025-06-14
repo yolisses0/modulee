34bb1f7f3e84f8db6dfc366f4826b23acd919518
Yolisses <yolisses0@gmail.com>
2025-06-14 09:10:25 -0300

    Rename GoogleSingInButton to WebGoogleSingInButton

--- src/lib/user/SignInPage.svelte
+++ src/lib/user/SignInPage.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { getHaveJuceSupport } from '$lib/engine/getHaveJuceSupport';
 	import Spinner from '$lib/ui/Spinner.svelte';
-	import GoogleSignInButton from './GoogleSignInButton.svelte';
 	import JuceGoogleSignInButton from './JuceGoogleSignInButton.svelte';
+	import WebGoogleSignInButton from './WebGoogleSignInButton.svelte';
 
 	let loading = $state(false);
 </script>
@@ -18,7 +18,7 @@
 			{:else if getHaveJuceSupport()}
 				<JuceGoogleSignInButton bind:loading />
 			{:else}
-				<GoogleSignInButton bind:loading />
+				<WebGoogleSignInButton bind:loading />
 			{/if}
 		</div>
 		<div class="h-10"></div>
