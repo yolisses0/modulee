f5538d775f0ae91af90534d9001e9c1ccbac068a
Yolisses <yolisses0@gmail.com>
2025-06-14 10:06:12 -0300

    Separate UserContent to deal with user id change

---
+++ src/lib/user/UserContent.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import type { ExternalModuleData } from '$lib/module/externalModule/ExternalModuleData';
+	import ExternalModuleItem from '$lib/module/externalModule/ExternalModuleItem.svelte';
+	import InfiniteList from '$lib/module/externalModule/InfiniteList.svelte';
+	import { Loader } from '$lib/module/externalModule/Loader.svelte';
+
+	interface Props {
+		userId: string;
+	}
+
+	type Group = 'created' | 'liked';
+	const { userId }: Props = $props();
+	let group = $state<Group>('created');
+
+	function getPath(loader: Loader<ExternalModuleData>) {
+		const queryParams = new URLSearchParams();
+
+		if (group === 'created') {
+			queryParams.append('userId', userId);
+			queryParams.append('sort', 'createdAt');
+		} else if (group === 'liked') {
+			queryParams.append('likedBy', userId);
+			// TODO sort by likedAt if group is 'liked'
+		}
+
+		if (loader.cursor) queryParams.append('cursor', loader.cursor);
+		const path = `/api/externalModules?${queryParams.toString()}`;
+		return path;
+	}
+	const loader = new Loader(getPath);
+
+	$effect(() => {
+		// on userId change
+		userId;
+		group = 'created';
+		loader.resetState();
+	});
+</script>
+
+<div class="flex flex-row">
+	<button
+		class="horizontal-tab"
+		onclick={() => {
+			group = 'created';
+			loader.resetState();
+		}}
+		class:horizontal-tab-selected={group === 'created'}
+	>
+		Created
+	</button>
+	<button
+		class="horizontal-tab"
+		onclick={() => {
+			group = 'liked';
+			loader.resetState();
+		}}
+		class:horizontal-tab-selected={group === 'liked'}
+	>
+		Liked
+	</button>
+</div>
+<hr class="opacity-10" />
+<InfiniteList {loader}>
+	{#snippet children(externalModuleData: ExternalModuleData)}
+		<ExternalModuleItem {externalModuleData} />
+	{/snippet}
+</InfiniteList>
--- src/lib/user/UserPage.svelte
+++ src/lib/user/UserPage.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import type { ExternalModuleData } from '$lib/module/externalModule/ExternalModuleData';
-	import ExternalModuleItem from '$lib/module/externalModule/ExternalModuleItem.svelte';
-	import InfiniteList from '$lib/module/externalModule/InfiniteList.svelte';
-	import { Loader } from '$lib/module/externalModule/Loader.svelte';
 	import ListPageLayout from '$lib/ui/ListPageLayout.svelte';
 	import EditUserButton from './EditUserButton.svelte';
 	import GuestUserWarn from './GuestUserWarn.svelte';
 	import LogoutButton from './LogoutButton.svelte';
+	import UserContent from './UserContent.svelte';
 	import type { UserData } from './UserData';
 	import { getUserDataContext } from './userDataContext';
 
@@ -17,26 +14,6 @@
 	const { userData }: Props = $props();
 	const userDataContext = getUserDataContext();
 	const isCurrentUser = $derived(userData.id === userDataContext.userData?.id);
-
-	type Group = 'created' | 'liked';
-	let group = $state<Group>('created');
-
-	function getPath(loader: Loader<ExternalModuleData>) {
-		const queryParams = new URLSearchParams();
-
-		if (group === 'created') {
-			queryParams.append('userId', userData.id);
-			queryParams.append('sort', 'createdAt');
-		} else if (group === 'liked') {
-			queryParams.append('likedBy', userData.id);
-			// TODO sort by likedAt if group is 'liked'
-		}
-
-		if (loader.cursor) queryParams.append('cursor', loader.cursor);
-		const path = `/api/externalModules?${queryParams.toString()}`;
-		return path;
-	}
-	const loader = new Loader(getPath);
 </script>
 
 <ListPageLayout title={userData.name}>
@@ -58,33 +35,6 @@
 		<GuestUserWarn />
 	{/if}
 	<div>
-		<div class="flex flex-row">
-			<button
-				class="horizontal-tab"
-				onclick={() => {
-					group = 'created';
-					loader.resetState();
-				}}
-				class:horizontal-tab-selected={group === 'created'}
-			>
-				Created
-			</button>
-			<button
-				class="horizontal-tab"
-				onclick={() => {
-					group = 'liked';
-					loader.resetState();
-				}}
-				class:horizontal-tab-selected={group === 'liked'}
-			>
-				Liked
-			</button>
-		</div>
-		<hr class="opacity-10" />
-		<InfiniteList {loader}>
-			{#snippet children(externalModuleData: ExternalModuleData)}
-				<ExternalModuleItem {externalModuleData} />
-			{/snippet}
-		</InfiniteList>
+		<UserContent userId={userData.id} />
 	</div>
 </ListPageLayout>
