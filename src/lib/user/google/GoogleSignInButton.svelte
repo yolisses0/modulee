81e5ab95017b4a576b3ea833670611b57d7a9657
Yolisses <yolisses0@gmail.com>
2025-06-14 11:40:04 -0300

    Use a blue div instead of spinner in GoogleSignInButton to decrease visual change

--- src/lib/user/google/GoogleSignInButton.svelte
+++ src/lib/user/google/GoogleSignInButton.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { getHaveJuceSupport } from '$lib/engine/getHaveJuceSupport';
-	import Spinner from '$lib/ui/Spinner.svelte';
 	import { onMount } from 'svelte';
 	import JuceGoogleSignInButton from './JuceGoogleSignInButton.svelte';
 	import WebGoogleSignInButton from './WebGoogleSignInButton.svelte';
@@ -15,7 +14,9 @@
 
 <div class="flex w-56 shrink-0 flex-col items-center">
 	{#if loading || haveJuceSuport === undefined}
-		<Spinner size={36} />
+		<div
+			class="flex h-10 w-52 cursor-pointer flex-row items-center justify-center gap-2 rounded bg-blue-600 p-0.5 pr-4 transition-colors duration-100 hover:bg-blue-500"
+		></div>
 	{:else if haveJuceSuport}
 		<JuceGoogleSignInButton bind:loading />
 	{:else}
