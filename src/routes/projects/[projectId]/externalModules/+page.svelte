82e5a68786ba71b04e4230996d32a1ecefd2fb74
Yolisses <yolisses0@gmail.com>
2025-06-13 21:54:08 -0300

    Remove projectNavbarSelectionContext usage from external modules page route

--- src/routes/projects/[projectId]/externalModules/+page.svelte
+++ src/routes/projects/[projectId]/externalModules/+page.svelte
@@ -1,9 +1,5 @@
 <script>
 	import ExternalModulesPage from '$lib/module/externalModule/ExternalModulesPage.svelte';
-	import { getProjectNavbarSelectionContext } from '$lib/project/projectNavbarSelectionContext';
-
-	const projectNavbarSelectionContext = getProjectNavbarSelectionContext();
-	projectNavbarSelectionContext.projectNavbarSelection = 'externalModules';
 </script>
 
 <ExternalModulesPage title="External modules" />
