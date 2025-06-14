5579d37c5a13ee34f75348c4483b2b546c61338f
Yolisses <yolisses0@gmail.com>
2025-06-14 11:42:51 -0300

    Make GoogleSignInButton more responsive

--- src/lib/user/google/GoogleSignInButton.svelte
+++ src/lib/user/google/GoogleSignInButton.svelte
@@ -12,7 +12,7 @@
 	});
 </script>
 
-<div class="flex w-56 shrink-0 flex-col items-center">
+<div class="flex min-w-56 shrink-0 flex-col items-center">
 	{#if loading || haveJuceSuport === undefined}
 		<div
 			class="flex h-10 w-52 cursor-pointer flex-row items-center justify-center gap-2 rounded bg-blue-600 p-0.5 pr-4 transition-colors duration-100 hover:bg-blue-500"
