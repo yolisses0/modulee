8473da097461e6d3ca8c84475f4be4cf4a19ea93
Yolisses <yolisses0@gmail.com>
2025-06-15 09:56:12 -0300

    Force typing in getExternalModuleData

--- src/lib/db/externalModule/getExternalModuleData.ts
+++ src/lib/db/externalModule/getExternalModuleData.ts
@@ -1,5 +1,6 @@
 import type { ExternalModuleData } from '$lib/module/externalModule/ExternalModuleData';
 import prisma from '$lib/prisma';
+import { error } from '@sveltejs/kit';
 
 export async function getExternalModuleData(externalModuleId: string): Promise<ExternalModuleData> {
 	const data = await prisma.externalModule.findUnique({
@@ -7,7 +8,7 @@ export async function getExternalModuleData(externalModuleId: string): Promise<E
 		where: { id: externalModuleId },
 	});
 	if (!data) {
-		throw new Error(`External module with id ${externalModuleId} not found`);
+		error(404, 'External module not found');
 	}
-	return data;
+	return data as unknown as ExternalModuleData;
 }
