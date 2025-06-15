964ced175bb2f982d0f28ed048fbee779f0d0086
Yolisses <yolisses0@gmail.com>
2025-06-15 07:23:44 -0300

    Create HelpPage placeholder

---
+++ src/lib/help/HelpPage.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import ListPageLayout from '$lib/ui/ListPageLayout.svelte';
+</script>
+
+<ListPageLayout title="Help"></ListPageLayout>
---
+++ src/routes/(home)/help/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import HelpPage from '$lib/help/HelpPage.svelte';
+	import { getHomeNavbarSelectionContext } from '$lib/home/homeNavbarSelectionContext';
+
+	getHomeNavbarSelectionContext().homeNavbarSelection = 'help';
+</script>
+
+<HelpPage />
