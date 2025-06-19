830fe5fc121d3c381b1d379c8cd8311e9998b2e4
Yolisses <yolisses0@gmail.com>
2025-06-19 10:38:04 -0300

    Fix import in ExternalModuleData

--- src/lib/module/externalModule/ExternalModuleData.ts
+++ src/lib/module/externalModule/ExternalModuleData.ts
@@ -1,6 +1,6 @@
 import type { GraphData } from '$lib/data/GraphData';
+import type { ModuleType } from '$lib/project/ModuleType';
 import type { UserData } from '$lib/user/UserData';
-import type { ModuleType } from '../../../generated/prisma';
 
 export type ExternalModuleData<T extends ModuleType = ModuleType> = {
 	id: string;
