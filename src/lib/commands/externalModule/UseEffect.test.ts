2fb5aab8887c4c503b5ce1542bb68f43e3b9d68c
Yolisses <yolisses0@gmail.com>
2025-06-19 04:18:58 -0300

    Refactor UseEffect

--- src/lib/commands/externalModule/UseEffect.test.ts
+++ src/lib/commands/externalModule/UseEffect.test.ts
@@ -1,23 +1,10 @@
 import type { GraphRegistry } from '$lib/data/GraphRegistry';
-import { ById } from '$lib/editor/ById';
-import { expect, test } from 'vitest';
+import { test } from 'vitest';
 import { mockCommandData } from '../test/mockNodeData';
 import { UseEffect } from './UseEffect';
 
 test('UseEffect', () => {
-	const graphRegistry = {
-		internalModules: ById.fromItems([{ id: 'internalModule1' }]),
-	} as GraphRegistry;
+	const graphRegistry = {} as GraphRegistry;
 
-	const command = new UseEffect(mockCommandData({ internalModule: { id: 'internalModule2' } }));
-	command.execute(graphRegistry);
-
-	expect(graphRegistry.internalModules.values()).toEqual([
-		{ id: 'internalModule1' },
-		{ id: 'internalModule2' },
-	]);
-
-	command.undo(graphRegistry);
-
-	expect(graphRegistry.internalModules.values()).toEqual([{ id: 'internalModule1' }]);
+	const command = mockCommandData<UseEffect>({});
 });
--- src/lib/commands/externalModule/UseEffect.ts
+++ src/lib/commands/externalModule/UseEffect.ts
@@ -7,6 +7,16 @@ import { AddNodeCommand } from '../node/AddNodeCommand';
 import { mockCommandData } from '../test/mockNodeData';
 import { SetExternalModuleReferenceCommand } from './SetExternalModuleReferenceCommand';
 
+/**
+ * Uses an effect following these steps:
+ *
+ * 1. Add the external module reference if not present.
+ * 2. Move the output node to add space to the effect module node.
+ * 3. Add the module node with the effect external module refference.
+ * 4. Connect the audio input nodes from the effect module node to the node
+ *    connected to the output node input if exists.
+ * 5. Connect the output node to the effect module node.
+ */
 export class UseEffect extends EditorCommand<{
 	connectionId: string;
 	moduleNodeId: string;
@@ -30,10 +40,20 @@ export class UseEffect extends EditorCommand<{
 			moduleNodePosition,
 		} = this.details;
 
+		// 1. Add the external module reference if not present.
 		this.setExternalModuleReferenceCommand = new SetExternalModuleReferenceCommand(
-			mockCommandData({ externalModuleReference: { type: 'external', id: externalModule.id } }),
+			mockCommandData({
+				externalModuleReference: {
+					type: 'external',
+					id: externalModule.id,
+				},
+			}),
 		);
+		this.setExternalModuleReferenceCommand.execute(graphRegistry);
 
+		// 2. Move the output node to add space to the effect module node.
+
+		// 3. Add the module node with the effect external module refference.
 		this.addNodeCommand = new AddNodeCommand(
 			mockCommandData({
 				node: {
@@ -42,24 +62,34 @@ export class UseEffect extends EditorCommand<{
 					type: 'ModuleNode',
 					unconnectedInputValues: {},
 					position: moduleNodePosition,
-					extras: { moduleReference: { type: 'external', id: externalModule.id } },
+					extras: {
+						moduleReference: {
+							type: 'external',
+							id: externalModule.id,
+						},
+					},
 				},
 			}),
 		);
+		this.addNodeCommand.execute(graphRegistry);
 
+		// 4. Connect the audio input nodes from the effect module node to the
+		//    node connected to the output node input if exists.
 		this.setConnectionCommand = new SetConnectionCommand(
 			mockCommandData({
 				connection: {
 					id: connectionId,
 					targetNodeId: moduleNodeId,
-					inputPath: { inputKey: 'input', nodeId: outputNodeId },
+					inputPath: {
+						inputKey: 'input',
+						nodeId: outputNodeId,
+					},
 				},
 			}),
 		);
-
-		this.addNodeCommand.execute(graphRegistry);
 		this.setConnectionCommand.execute(graphRegistry);
-		this.setExternalModuleReferenceCommand.execute(graphRegistry);
+
+		// 5. Connect the output node to the effect module node.
 	}
 
 	undo(graphRegistry: GraphRegistry): void {
