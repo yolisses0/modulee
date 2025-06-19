71adb8a11585e19c9dd894f93b73b93d48d3a7c9
Yolisses <yolisses0@gmail.com>
2025-06-19 10:42:01 -0300

    Fix cloneGraphRegistry

--- src/lib/process/cloneGraphRegistry.ts
+++ src/lib/process/cloneGraphRegistry.ts
@@ -1,12 +1,19 @@
 import type { GraphRegistry } from '$lib/data/GraphRegistry';
 
 export function cloneGraphRegistry(graphRegistry: GraphRegistry): GraphRegistry {
-	const { mainInternalModuleId, connections, externalModuleReferences, internalModules, nodes } =
-		graphRegistry;
+	const {
+		nodes,
+		connections,
+		externalModules,
+		internalModules,
+		mainInternalModuleId,
+		externalModuleReferences,
+	} = graphRegistry;
 	return {
 		nodes: nodes.structuredClone(),
 		connections: connections.structuredClone(),
 		mainInternalModuleId: mainInternalModuleId,
+		externalModules: externalModules.structuredClone(),
 		internalModules: internalModules.structuredClone(),
 		externalModuleReferences: externalModuleReferences.structuredClone(),
 	};
