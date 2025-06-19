0e8ca75f59e5de2055abdf38fd25c7791a240e15
Yolisses <yolisses0@gmail.com>
2025-06-19 09:08:30 -0300

    Create AddNodeMonkey and RemoveNodeMonkey

--- src/lib/monkey/editorMonkey.test.ts
+++ src/lib/monkey/editorMonkey.test.ts
@@ -1,8 +1,63 @@
+import { AddNodeCommand } from '$lib/commands/node/AddNodeCommand';
+import { RemoveNodeCommand } from '$lib/commands/node/RemoveNodeCommand';
 import { createId } from '$lib/data/createId';
 import type { GraphRegistry } from '$lib/data/GraphRegistry';
 import type { InternalModuleData } from '$lib/data/InternalModuleData';
 import { ById } from '$lib/editor/ById';
+import type { EditorCommand } from '$lib/editor/EditorCommand';
+import { createFakePosition } from '$lib/fake/createFakePosition';
+import { getRandomItem } from '$lib/fake/getRandomItem';
+import { nodeDefinitions } from '$lib/node/definitions/nodeDefinitions';
+import { getNodeDataFromNodeDefinition } from '$lib/process/implicitNodes/addImplicitNodes';
 import { test } from 'vitest';
+
+const FAKE_PROJECT_ID = createId();
+
+abstract class Monkey {
+	abstract getCanBeUsed(graphRegistry: GraphRegistry): boolean;
+	abstract createCommand(graphRegistry: GraphRegistry): EditorCommand;
+}
+
+class AddNodeMonkey extends Monkey {
+	getCanBeUsed(graphRegistry: GraphRegistry): boolean {
+		return graphRegistry.internalModules.values().length > 0;
+	}
+
+	createCommand(graphRegistry: GraphRegistry) {
+		const nodeDefinition = getRandomItem(nodeDefinitions);
+		const node = getNodeDataFromNodeDefinition(
+			createId(),
+			nodeDefinition,
+			getRandomItem(graphRegistry.internalModules.ids()),
+			createFakePosition(),
+		);
+		return new AddNodeCommand({
+			id: createId(),
+			details: { node },
+			type: 'AddNodeCommand',
+			projectId: FAKE_PROJECT_ID,
+			createdAt: new Date().toJSON(),
+		});
+	}
+}
+
+class RemoveNodeMonkey extends Monkey {
+	getCanBeUsed(graphRegistry: GraphRegistry): boolean {
+		return graphRegistry.nodes.values().length > 0;
+	}
+
+	createCommand(graphRegistry: GraphRegistry) {
+		return new RemoveNodeCommand({
+			id: createId(),
+			type: 'AddNodeCommand',
+			projectId: FAKE_PROJECT_ID,
+			createdAt: new Date().toJSON(),
+			details: { nodeId: getRandomItem(graphRegistry.nodes.values()).id },
+		});
+	}
+}
+
+const monkeys = [AddNodeMonkey, RemoveNodeMonkey];
 
 test('editorMonkey', () => {
 	// By now, every graph needs a main internal module. It can be changed
