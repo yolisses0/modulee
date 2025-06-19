348301e2a7e9154b8a0243280eaf46ef12d83240
Yolisses <yolisses0@gmail.com>
2025-06-19 12:59:02 -0300

    Create RedoMonkey

---
+++ src/lib/commands/editor/RedoMonkey.ts
@@ -0,0 +1,26 @@
+import { createId } from '$lib/data/createId';
+import type { GraphRegistry } from '$lib/data/GraphRegistry';
+import type { EditorData } from '$lib/editor/EditorData';
+import { EditorMonkey } from '$lib/monkey/EditorMonkey';
+import { FAKE_PROJECT_ID } from '$lib/monkey/FAKE_PROJECT_ID';
+import { RedoCommand } from './RedoCommand';
+
+export class RedoMonkey extends EditorMonkey {
+	getCanBeUsed(graphRegistry: GraphRegistry, editorData: EditorData): boolean {
+		return editorData.undoneHistory.length > 0;
+	}
+
+	createCommand(graphRegistry: GraphRegistry, editorData: EditorData) {
+		const lastCommand = editorData.undoneHistory.at(-1);
+		if (!lastCommand) {
+			throw new Error('Missing last command');
+		}
+		return new RedoCommand({
+			id: createId(),
+			type: 'RedoCommand',
+			projectId: FAKE_PROJECT_ID,
+			createdAt: new Date().toJSON(),
+			details: { commandId: lastCommand.id },
+		});
+	}
+}
--- src/lib/monkey/editorMonkeys.ts
+++ src/lib/monkey/editorMonkeys.ts
@@ -1,5 +1,6 @@
 import { DisconnectMonkey } from '$lib/commands/connection/DisconnectMonkey';
 import { SetConnectionMonkey } from '$lib/commands/connection/SetConnectionMonkey';
+import { RedoMonkey } from '$lib/commands/editor/RedoMonkey';
 import { UndoMonkey } from '$lib/commands/editor/UndoMonkey';
 import { AddNodeMonkey } from '$lib/commands/node/AddNodeMonkey';
 import { RemoveNodeMonkey } from '$lib/commands/node/RemoveNodeMonkey';
@@ -7,6 +8,7 @@ import { AddInternalModuleMonkey } from '../commands/internalModule/AddInternalM
 import { RemoveInternalModuleMonkey } from '../commands/internalModule/RemoveInternalModuleMonkey';
 
 export const editorMonkeys = [
+	RedoMonkey,
 	UndoMonkey,
 	AddNodeMonkey,
 	DisconnectMonkey,
