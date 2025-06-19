7428885ad1b80ed2a09a62def549676526de8ad4
Yolisses <yolisses0@gmail.com>
2025-06-19 13:27:23 -0300

    Fix createNodeData test

--- src/lib/node/add/createNodeData.test.ts
+++ src/lib/node/add/createNodeData.test.ts
@@ -1,9 +1,10 @@
+import { createId } from '$lib/data/createId';
 import { test } from 'vitest';
 import { nodeDefinitions } from '../definitions/nodeDefinitions';
 import { createNodeData } from './createNodeData';
 
 test('createNodeData', () => {
 	nodeDefinitions.forEach((nodeDefinition) => {
-		createNodeData(nodeDefinition, 'someId', { x: 1, y: 2 });
+		createNodeData(nodeDefinition, createId(), { x: 1, y: 2 });
 	});
 });
