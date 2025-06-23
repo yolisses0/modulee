40e0db4c9da9cb6b7a3bfac04d1c5ccd3e18c50a
Yolisses <yolisses0@gmail.com>
2025-06-23 16:35:52 -0300

    Use DeleteEffectModuleNode in DeleteEffectModuleNodeButton

--- src/lib/rack/DeleteEffectModuleNodeButton.svelte
+++ src/lib/rack/DeleteEffectModuleNodeButton.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { DeleteEffectModuleNode } from '$lib/commands/externalModule/DeleteEffectModuleNode';
+	import { createId } from '$lib/data/createId';
 	import type { ModuleNode } from '$lib/data/ModuleNode.svelte';
+	import { getEditorContext } from '$lib/editor/editorContext';
+	import { getProjectDataContext } from '$lib/project/projectDataContext';
 	import { faTrash } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
@@ -8,11 +12,23 @@
 	}
 
 	const { moduleNode }: Props = $props();
+	const editorContext = getEditorContext();
+	const projectDataContext = getProjectDataContext();
 
-	function handleClick() {}
+	function handleClick() {
+		const command = new DeleteEffectModuleNode({
+			id: createId(),
+			createdAt: new Date().toJSON(),
+			type: 'DeleteEffectModuleNode',
+			details: { moduleNodeId: moduleNode.id },
+			projectId: projectDataContext.projectData.id,
+		});
+
+		editorContext.editor.execute(command);
+	}
 </script>
 
-<button class="common-button">
+<button class="common-button" onclick={handleClick}>
 	<Fa icon={faTrash} />
 	Remove effect
 </button>
