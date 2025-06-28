018b7bf41c574f059e6acc8ecc620119563ebf01
Yolisses <yolisses0@gmail.com>
2025-06-28 01:44:03 -0300

    Start creating ReorderEffectCommand test

---
+++ src/lib/commands/externalModule/ReorderEffectCommand.test.ts
@@ -0,0 +1,30 @@
+import type { GraphRegistry } from '$lib/data/GraphRegistry';
+import { ById } from '$lib/editor/ById';
+import { expect, test } from 'vitest';
+import { mockCommandData } from '../test/mockNodeData';
+import { ReorderEffectCommand } from './ReorderEffectCommand';
+
+test('ReorderEffectCommand', () => {
+	const graphRegistry = {
+		nodes: ById.fromItems([
+			{ id: 'node1' },
+			{ id: 'node2' },
+			{ id: 'node3' },
+			{ id: 'node4' },
+			{ id: 'node5' },
+		]),
+		connections: ById.fromItems([
+			{ id: 'connection1', targetNodeId: 'node1', inputPath: { nodeId: 'node2' } },
+			{ id: 'connection2', targetNodeId: 'node2', inputPath: { nodeId: 'node3' } },
+			{ id: 'connection3', targetNodeId: 'node3', inputPath: { nodeId: 'node4' } },
+			{ id: 'connection4', targetNodeId: 'node4', inputPath: { nodeId: 'node5' } },
+		]),
+	} as GraphRegistry;
+
+	const command = new ReorderEffectCommand(mockCommandData({ moduleNodeId: 'node2' }));
+
+	command.execute(graphRegistry);
+
+	expect(graphRegistry.connections.get('connection1').targetNodeId).toBe('node1');
+	expect(graphRegistry.connections.get('connection4').targetNodeId).toBe('node2');
+});
--- src/lib/commands/externalModule/ReorderEffectCommand.ts
+++ src/lib/commands/externalModule/ReorderEffectCommand.ts
@@ -1,6 +1,13 @@
 import type { GraphRegistry } from '$lib/data/GraphRegistry';
 import { EditorCommand } from '$lib/editor/EditorCommand';
 
+/**
+ * Move an effect in the effect chain:
+ *
+ * 1. Replace audio input connections targets from the module node to its target
+ *    node (if any).
+ * 2. Replace
+ */
 export class ReorderEffectCommand extends EditorCommand<{
 	moduleNodeId: string;
 }> {
