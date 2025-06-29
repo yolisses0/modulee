d45a84b779fdf90ccd41c5ba9c491853540169db
Yolisses <yolisses0@gmail.com>
2025-06-29 12:17:38 -0300

    Add chain divisions in RackModuleList

---
+++ src/lib/rack/ChainDivision.svelte
@@ -0,0 +1,1 @@
+<div class="w-1 rounded bg-white/10" title="Chain division"></div>
--- src/lib/rack/RackModuleList.svelte
+++ src/lib/rack/RackModuleList.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { expandById } from '$lib/array/expandById';
+	import type { TopologicalMap } from '$lib/connection/TopologicalMap';
 	import type { ModuleNode } from '$lib/node/ModuleNode.svelte';
 	import Sortable from 'sortablejs';
 	import { onMount } from 'svelte';
+	import ChainDivision from './ChainDivision.svelte';
+	import { getChains } from './getChains';
 	import { hideDraggingImage } from './hideDraggingImage';
 	import RackModuleNodeItem from './RackModuleNodeItem.svelte';
 
@@ -11,6 +15,23 @@
 
 	let element = $state<HTMLElement>();
 	const { moduleNodes }: Props = $props();
+	// moduleNodes have to be topologically sorted
+
+	function getAudioTopologicalMap(moduleNodes: ModuleNode[]) {
+		const graph: TopologicalMap = new Map();
+		moduleNodes.forEach((moduleNode) => {
+			const inputs = [];
+			const targetNodeId = moduleNode.audioInput?.targetNode?.id;
+			if (targetNodeId && moduleNodes.some((moduleNode) => moduleNode.id === targetNodeId)) {
+				inputs.push(targetNodeId);
+			}
+			graph.set(moduleNode.id, inputs);
+		});
+		return graph;
+	}
+
+	const idChains = getChains(getAudioTopologicalMap(moduleNodes));
+	const chains = idChains.map((idChain) => expandById(moduleNodes, idChain));
 
 	onMount(() => {
 		if (element) {
@@ -25,7 +46,12 @@
 </script>
 
 <div class="flex flex-row flex-wrap justify-center gap-1 p-1" bind:this={element}>
-	{#each moduleNodes as moduleNode (moduleNode.id)}
-		<RackModuleNodeItem {moduleNode} />
+	{#each chains as chain, index}
+		{#each chain as moduleNode (moduleNode.id)}
+			<RackModuleNodeItem {moduleNode} />
+		{/each}
+		{#if index < chains.length - 1}
+			<ChainDivision />
+		{/if}
 	{/each}
 </div>
