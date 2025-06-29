85b51597cd2a3668adb2442dbfdc3da57e58b51c
Yolisses <yolisses0@gmail.com>
2025-06-28 21:45:16 -0300

    Refactor createId test

--- src/lib/data/createId.test.ts
+++ src/lib/data/createId.test.ts
@@ -1,7 +1,8 @@
-import { expect, test } from 'vitest';
+import { test } from 'vitest';
+import { z } from 'zod/v4';
 import { createId } from './createId';
 
 test('createId', () => {
 	const id = createId();
-	expect(typeof id).toBe('string');
+	z.uuid().parse(id);
 });
