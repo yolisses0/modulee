5c08597a4ed6d61edd1b09de9e95c4e5f49645b6
Yolisses <yolisses0@gmail.com>
2025-07-01 07:36:36 -0300

    Use some module data instead of only module data in get is audio connection

--- src/lib/connection/getIsAudioConnectionData.ts
+++ src/lib/connection/getIsAudioConnectionData.ts
@@ -1,14 +1,19 @@
 import type { ConnectionData } from '$lib/connection/ConnectionData';
 import type { GraphRegistry } from '$lib/graph/GraphRegistry';
 import { AUDIO_INPUT_KEY } from '$lib/input/AUDIO_INPUT_KEY';
+import { getIsSomeModuleNodeData } from '$lib/rack/getIsSomeModuleNodeData';
 
 export function getIsAudioConnectionData(
 	connectionData: ConnectionData,
 	graphRegistry: GraphRegistry,
 ) {
+	const targetNode = graphRegistry.nodes.getOrNull(connectionData.targetNodeId);
+	const originNode = graphRegistry.nodes.getOrNull(connectionData.inputPath.nodeId);
 	return (
 		connectionData.inputPath.inputKey === AUDIO_INPUT_KEY &&
-		graphRegistry.nodes.getOrNull(connectionData.targetNodeId)?.type === 'ModuleNode' &&
-		graphRegistry.nodes.getOrNull(connectionData.inputPath.nodeId)?.type === 'ModuleNode'
+		targetNode &&
+		getIsSomeModuleNodeData(targetNode) &&
+		originNode &&
+		getIsSomeModuleNodeData(originNode)
 	);
 }
