9dc5af5d58aa22c5d3f6d553047ea970c65c77a5
Yolisses <yolisses0@gmail.com>
2025-07-06 19:35:15 -0300

    Separate PreviewConnectionEndHandler

--- src/lib/graph/GraphCanvasPointerStrategyFactory.svelte.ts
+++ src/lib/graph/GraphCanvasPointerStrategyFactory.svelte.ts
@@ -1,20 +1,15 @@
-import { DisconnectCommand } from '$lib/commands/connection/DisconnectCommand';
-import { SetConnectionCommand } from '$lib/commands/connection/SetConnectionCommand';
 import { editorContextKey } from '$lib/editor/editorContext';
-import { createId } from '$lib/global/createId';
 import { getRequiredContext } from '$lib/global/getRequiredContext';
-import type { InputPath } from '$lib/input/InputPath';
 import { addNodeMenuParamsContextKey } from '$lib/node/add/addNodeMenuParamsContext';
-import { getInputAndOutput } from '$lib/node/getInputAndOutput';
 import { projectDataContextKey } from '$lib/project/ui/projectDataContext';
 import { spaceContextKey } from '$lib/space/spaceContext';
 import {
 	previewConnectionContextKey,
 	PreviewConnectionPointerStrategy,
 	SelectionBoxPointerStrategy,
-	type EndPreviewConnectionEvent,
 } from 'nodes-editor';
 import { graphContextKey } from './graphContext';
+import { PreviewConnectionEndHandler } from './PreviewConnectionEndHandler';
 
 export class GraphCanvasPointerStrategyFactory {
 	graphContext = getRequiredContext(graphContextKey);
@@ -23,49 +18,11 @@ export class GraphCanvasPointerStrategyFactory {
 	projectDataContext = getRequiredContext(projectDataContextKey);
 	previewConnectionContext = getRequiredContext(previewConnectionContextKey);
 	addNodeMenuParamsContext = getRequiredContext(addNodeMenuParamsContextKey);
+	previewConnectionEndHandler: PreviewConnectionEndHandler;
 
-	handleEndPreviewConnection = (e: EndPreviewConnectionEvent) => {
-		const { input, output } = getInputAndOutput(e);
-		if (!input) return;
-
-		const inputPath: InputPath = {
-			inputKey: input.key,
-			nodeId: input.node.id,
-		};
-
-		if (output) {
-			const command = new SetConnectionCommand({
-				id: createId(),
-				type: 'SetConnectionCommand',
-				createdAt: new Date().toJSON(),
-				projectId: this.projectDataContext.projectData.id,
-				details: {
-					connection: {
-						inputPath,
-						id: createId(),
-						targetNodeId: output?.node.id,
-					},
-				},
-			});
-			this.editorContext.editor.execute(command);
-		} else {
-			const command = new DisconnectCommand({
-				id: createId(),
-				details: { inputPath },
-				type: 'DisconnectCommand',
-				createdAt: new Date().toJSON(),
-				projectId: this.projectDataContext.projectData.id,
-			});
-			this.editorContext.editor.execute(command);
-
-			const screenPosition = e.mousePosition;
-			const dataPosition = this.spaceContext.space.getDataPosition(screenPosition);
-			this.addNodeMenuParamsContext.addNodeMenuParams = {
-				input,
-				position: dataPosition,
-			};
-		}
-	};
+	constructor() {
+		this.previewConnectionEndHandler = new PreviewConnectionEndHandler();
+	}
 
 	getPointerStrategy() {
 		const selectionBoxPointerStrategy = new SelectionBoxPointerStrategy((e) => {
@@ -73,7 +30,7 @@ export class GraphCanvasPointerStrategyFactory {
 			return e.button !== 2;
 		});
 		const previewConnectionPointerStrategy = new PreviewConnectionPointerStrategy(
-			this.handleEndPreviewConnection,
+			this.previewConnectionEndHandler.handleEndPreviewConnection,
 		);
 
 		const pointerStrategy = this.previewConnectionContext.startConnector
