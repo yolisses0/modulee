1d272a2c26c1842c7bb5804326e282578ce40a90
Yolisses <yolisses0@gmail.com>
2025-07-06 04:12:23 -0300

    Set homeNavbarSelectionContext on plugin page

--- src/routes/(home)/plugin/+page.svelte
+++ src/routes/(home)/plugin/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+	import { getRequiredContext } from '$lib/global/getRequiredContext';
+	import { homeNavbarSelectionContextKey } from '$lib/home/homeNavbarSelectionContext';
 	import PluginPage from '$lib/plugin/PluginPage.svelte';
+
+	getRequiredContext(homeNavbarSelectionContextKey).homeNavbarSelection = 'plugin';
 </script>
 
 <PluginPage />
