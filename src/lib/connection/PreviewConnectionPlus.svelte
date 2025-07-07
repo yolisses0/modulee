80ea7775175e16e0590bb4b77361f52a4497c0be
Yolisses <yolisses0@gmail.com>
2025-07-07 15:19:47 -0300

    Add stroke in preview connection plus

--- src/lib/connection/PreviewConnectionPlus.svelte
+++ src/lib/connection/PreviewConnectionPlus.svelte
@@ -9,10 +9,31 @@
 
 	const long = 12;
 	const short = 3;
-	const rounded = 2;
-	const offset = 16;
+	const rounded = 1;
+	const offset = 10;
 </script>
 
+<!-- TODO consider using a path if not too complicated -->
+<rect
+	rx={rounded}
+	fill="white"
+	width="{short}px"
+	height="{long}px"
+	stroke="black"
+	stroke-width="3"
+	x={position.x - offset}
+	y={position.y - offset - long / 2 + short / 2}
+></rect>
+<rect
+	rx={rounded}
+	fill="white"
+	width="{long}px"
+	height="{short}px"
+	stroke="black"
+	stroke-width="3"
+	x={position.x - offset - long / 2 + short / 2}
+	y={position.y - offset}
+></rect>
 <rect
 	rx={rounded}
 	fill="white"
