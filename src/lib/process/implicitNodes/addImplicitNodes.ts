59dc688618eeb85e4b5e8885cd0004a0168aad25
Yolisses <yolisses0@gmail.com>
2025-07-11 18:58:28 -0300

    Refactor addImplicitNodes

--- src/lib/process/implicitNodes/addImplicitNodes.ts
+++ src/lib/process/implicitNodes/addImplicitNodes.ts
@@ -17,68 +17,61 @@ function getImplicitNodeId(inputPath: InputPath) {
 	return 'implicit_node_for_input_path_' + getInputPathId(inputPath);
 }
 
-export function createInputImplicitNode(
+function createImplicitNode(
 	inputPath: InputPath,
-	nodeData: NodeData,
+	internalModuleId: string,
 	nodeDefinition: NodeDefinition,
-) {
+): NodeData {
 	const positionData: VectorData = { x: 0, y: 0 };
-	const implicitNodeData: NodeData = createNodeData(
-		nodeDefinition,
-		nodeData.internalModuleId,
-		positionData,
-	);
-	implicitNodeData.id = getImplicitNodeId(inputPath);
-	return implicitNodeData;
+	const implicitNode: NodeData = createNodeData(nodeDefinition, internalModuleId, positionData);
+	implicitNode.id = getImplicitNodeId(inputPath);
+	return implicitNode;
 }
 
-function createImplicitConnection(
-	inputPath: InputPath,
-	implicitNodeData: NodeData,
-): ConnectionData {
+function createImplicitConnection(inputPath: InputPath, implicitNode: NodeData): ConnectionData {
 	return {
 		id: getImplicitConnectionId(inputPath),
-		inputPath: inputPath,
-		targetNodeId: implicitNodeData.id,
+		inputPath,
+		targetNodeId: implicitNode.id,
 	};
 }
 
-export function addInputImplicitNode(
+function addImplicitNodeForInput(
 	inputPath: InputPath,
-	nodeData: NodeData,
+	internalModuleId: string,
 	graphRegistry: GraphRegistry,
 	nodeDefinition: NodeDefinition,
-) {
-	const implicitNodeData = createInputImplicitNode(inputPath, nodeData, nodeDefinition);
-	graphRegistry.nodes.add(implicitNodeData);
+): void {
+	const implicitNode = createImplicitNode(inputPath, internalModuleId, nodeDefinition);
+	graphRegistry.nodes.add(implicitNode);
 
-	const implicitConnectionData: ConnectionData = createImplicitConnection(
-		inputPath,
-		implicitNodeData,
-	);
-	graphRegistry.connections.add(implicitConnectionData);
+	const implicitConnection = createImplicitConnection(inputPath, implicitNode);
+	graphRegistry.connections.add(implicitConnection);
 
-	addNodeImplicitNodes(implicitNodeData, graphRegistry);
+	addImplicitNodesForNode(implicitNode, graphRegistry);
 }
 
-export function addNodeImplicitNodes(nodeData: NodeData, graphRegistry: GraphRegistry) {
+function addImplicitNodesForNode(nodeData: NodeData, graphRegistry: GraphRegistry): void {
 	const nodeDefinition = nodeDefinitionsByName[nodeData.type];
+	if (!nodeDefinition) return;
+
 	nodeDefinition.inputs.forEach((inputDefinition) => {
+		if (!inputDefinition.autoConnection) return;
 		if (!nodeData.isInputAutoConnectedMap[inputDefinition.key]) return;
 
-		if (!inputDefinition.autoConnection) return;
 		const inputPath: InputPath = { nodeId: nodeData.id, inputKey: inputDefinition.key };
 		if (getIsInputConnected(inputPath, graphRegistry)) return;
 
 		const implicitNodeDefinition = nodeDefinitionsByName[inputDefinition.autoConnection.nodeType];
 		if (!implicitNodeDefinition) return;
 
-		addInputImplicitNode(inputPath, nodeData, graphRegistry, implicitNodeDefinition);
+		const { internalModuleId } = nodeData;
+		addImplicitNodeForInput(inputPath, internalModuleId, graphRegistry, implicitNodeDefinition);
 	});
 }
 
-export function addImplicitNodes(graphRegistry: GraphRegistry) {
-	graphRegistry.nodes.values().forEach((nodeData) => {
-		addNodeImplicitNodes(nodeData, graphRegistry);
+export function addImplicitNodes(graphRegistry: GraphRegistry): void {
+	graphRegistry.nodes.values().forEach((node) => {
+		addImplicitNodesForNode(node, graphRegistry);
 	});
 }
