f33e997cedd2a948470ead38e6a8821a013fe157
Yolisses <yolisses0@gmail.com>
2025-07-14 17:48:46 -0300

    Implement formatNodes for two connected nodes

--- src/lib/graph/format/formatNodes.test.ts
+++ src/lib/graph/format/formatNodes.test.ts
@@ -4,15 +4,36 @@ import type { FormatingNode } from './FormatingNode';
 import { formatNodes } from './formatNodes';
 
 describe('formatNodes', () => {
+	const xStep = 1;
+	const getNextY = () => 1;
+
 	test('zero nodes', () => {
 		const nodes: FormatingNode[] = [];
-		const positions = formatNodes(nodes);
+		const positions = formatNodes(nodes, getNextY, xStep);
 		expect(positions).toEqual([]);
 	});
 
-	test('zero nodes', () => {
+	test('one node', () => {
 		const nodes: FormatingNode[] = [{ id: 'node1', inputs: [] }];
-		const positions = formatNodes(nodes);
+		const positions = formatNodes(nodes, getNextY, xStep);
 		expect(positions).toEqual([new Vector(0, 0)]);
+	});
+
+	test('two separate nodes', () => {
+		const nodes: FormatingNode[] = [
+			{ id: 'node1', inputs: [] },
+			{ id: 'node2', inputs: [] },
+		];
+		const positions = formatNodes(nodes, getNextY, xStep);
+		expect(positions).toEqual([new Vector(0, 0), new Vector(0, 1)]);
+	});
+
+	test.only('two connected nodes', () => {
+		const nodes: FormatingNode[] = [
+			{ id: 'node1', inputs: [] },
+			{ id: 'node2', inputs: ['node1'] },
+		];
+		const positions = formatNodes(nodes, getNextY, xStep);
+		expect(positions).toEqual([new Vector(0, 0), new Vector(1, 0)]);
 	});
 });
--- src/lib/graph/format/formatNodes.ts
+++ src/lib/graph/format/formatNodes.ts
@@ -2,17 +2,27 @@ import { ById } from '$lib/editor/ById';
 import { Vector } from 'nodes-editor';
 import type { FormatingNode } from './FormatingNode';
 
+type GetNextY = (node: FormatingNode) => number;
+
 // TODO consider returning the positions instead of changing them in place
-export function formatNodes(nodes: FormatingNode[]) {
+export function formatNodes(nodes: FormatingNode[], getNextY: GetNextY, xStep: number) {
+	const nextPositionsByLayer = [0];
 	const nodesById = ById.fromItems(nodes);
 	const positions = new Map<FormatingNode, Vector>();
 
 	function visit(node: FormatingNode, parent: FormatingNode | null, layer: number) {
 		if (positions.has(node)) return;
 
-		if (layer === 0) {
-			positions.set(node, Vector.zero());
+		const x = layer * xStep;
+
+		if (nextPositionsByLayer[layer] === undefined) {
+			nextPositionsByLayer[layer] = 0;
 		}
+		const y = nextPositionsByLayer[layer];
+		console.log(node, parent, layer, x);
+		const position = new Vector(x, y);
+		positions.set(node, position);
+		nextPositionsByLayer[layer] = getNextY(node);
 
 		node.inputs.forEach((input) => {
 			const nextNode = nodesById.get(input);
@@ -20,7 +30,7 @@ export function formatNodes(nodes: FormatingNode[]) {
 		});
 	}
 
-	nodes.forEach((node) => {
+	nodes.toReversed().forEach((node) => {
 		visit(node, null, 0);
 	});
 
