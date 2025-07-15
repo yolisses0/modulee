9560fae66b1226e1de4b34c0e693ed0ac3a986e8
Yolisses <yolisses0@gmail.com>
2025-07-14 21:34:25 -0300

    Format projects/+page.server.ts

--- src/routes/(home)/projects/+page.server.ts
+++ src/routes/(home)/projects/+page.server.ts
@@ -11,7 +11,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const { userId } = getSession(locals);
-
 	const projects = await getProjects(userId);
 	return { projects };
 };
