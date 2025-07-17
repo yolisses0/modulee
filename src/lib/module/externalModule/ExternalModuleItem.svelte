b0bd8b4757cf23cb2843da01159f41fd7edb4f6c
Yolisses <yolisses0@gmail.com>
2025-07-17 18:55:50 -0300

    Bring use button closer in external module item

--- src/lib/module/externalModule/ExternalModuleItem.svelte
+++ src/lib/module/externalModule/ExternalModuleItem.svelte
@@ -15,30 +15,28 @@
 </script>
 
 <div class="border-b border-white/10 p-2 pb-6 last:border-none">
-	<div class="flex flex-row items-center">
-		<div class="flex-1">
-			<a
-				class="hover:underline"
-				href="{baseRouteContext.baseRoute}/externalModules/{externalModuleData.id}?closePath={page.url}"
-			>
-				{externalModuleData.name}
-			</a>
-		</div>
-		<LikeButton externalModuleId={externalModuleData.id} />
-		<UseExternalModuleButton {externalModuleData} />
-	</div>
+	<a
+		class="hover:underline"
+		href="{baseRouteContext.baseRoute}/externalModules/{externalModuleData.id}?closePath={page.url}"
+	>
+		{externalModuleData.name}
+	</a>
 	<div class="block max-h-10 overflow-hidden text-sm text-ellipsis text-white/75">
 		{externalModuleData.description}
 	</div>
 	<div class="text-sm opacity-50">
-		By
+		Created at {new Date(externalModuleData.createdAt).toLocaleDateString()}
+		• By
 		<a
 			class="hover:underline"
 			href="{baseRouteContext.baseRoute}/users/{externalModuleData.userId}?closePath={page.url}"
 		>
 			{externalModuleData.user?.username}
 		</a>
-		• Created at {new Date(externalModuleData.createdAt).toLocaleDateString()}
 		• Liked {externalModuleData.likeCount} times
+	</div>
+	<div class="flex flex-row">
+		<UseExternalModuleButton {externalModuleData} />
+		<LikeButton externalModuleId={externalModuleData.id} />
 	</div>
 </div>
