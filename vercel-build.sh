d478d02727c884e1bff1fe490605828d239448a3
Yolisses <yolisses0@gmail.com>
2025-08-17 19:43:16 -0300

    Try debugging vercel deploy again

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,6 +1,6 @@
 npx prisma migrate deploy&&
 npx prisma generate&&
 # Debugging where to put the prisma missing file
-find /vercel -maxdepth 3 -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'&&
-cp node_modules/prisma/libquery_engine-rhel-openssl-3.0.x.so.node /vercel/path0/.svelte-kit/output/server/chunks &
+ls -la
+cp node_modules/prisma/libquery_engine-rhel-openssl-3.0.x.so.node /var/task/vercel/path0/.svelte-kit/output/server/chunks &
 npm run build;
