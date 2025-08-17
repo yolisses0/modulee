12016a6657a9cd7f8bb494c139621d1973552d66
Yolisses <yolisses0@gmail.com>
2025-08-17 17:22:29 -0300

    Create vercel-build.sh

---
+++ vercel-build.sh
@@ -0,0 +1,4 @@
+npx prisma migrate deploy&&
+npx prisma generate&&
+cp node_modules/prisma/libquery_engine-rhel-openssl-3.0.x.so.node /var/task/vercel/path0/.svelte-kit/output/server/chunks/&
+npm run build;
