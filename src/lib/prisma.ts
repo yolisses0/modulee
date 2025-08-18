d32eb2ae151ce549179862f852d3b8801bc6f343
Yolisses <yolisses0@gmail.com>
2025-08-17 22:37:01 -0300

    Disable Rust code of Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,13 @@
 import { DATABASE_URL } from '$env/static/private';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 
+const adapter = new PrismaPg({
+	connectionString: DATABASE_URL,
+});
+
 const prisma = new PrismaClient({
-	datasourceUrl: DATABASE_URL,
+	adapter,
 	omit: {
 		externalModule: { isForDevTesting: true },
 		user: { email: true, isForDevTesting: true },
