dacac366dc65bee28eb26fc18c28f4c286a3fca0
Yolisses <yolisses0@gmail.com>
2025-08-22 11:17:26 -0300

    Install devtoolsJson

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	test: {
 		workspace: [
 			{
