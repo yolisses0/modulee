1279ba3e08de502b072d534b52899ada72afecf3
Yolisses <yolisses0@gmail.com>
2025-08-23 11:08:41 -0300

    Prevent duplication of module nodes

--- src/lib/process/makeStereo/makeStereo.ts
+++ src/lib/process/makeStereo/makeStereo.ts
@@ -1,5 +1,6 @@
 import { createId } from '$lib/global/createId';
 import type { GraphRegistry } from '$lib/graph/GraphRegistry';
+import { getIsSomeModuleNodeData } from '$lib/rack/getIsSomeModuleNodeData';
 
 const STEREO_PREFIX = '/channel1';
 
@@ -14,20 +15,35 @@ const STEREO_PREFIX = '/channel1';
 // graphs with nested modules, it is required to use a "meta" node that will be
 // equivalent to creating a second output for the module nodes. Only modules
 // with stereo output will have this attached to.
+//
+// The rules
+// - A node inherently stereo should be duplicated (e.g.: NoiseNode)
+// - A node that depends on a inherently stereo node should be duplicated
+// - Module nodes should not be duplicated
 export function makeStereo(graphRegistry: GraphRegistry) {
+	const idsMap: Record<string, string> = {};
+
 	graphRegistry.nodes.values().forEach((nodeData) => {
+		if (getIsSomeModuleNodeData(nodeData)) {
+			idsMap[nodeData.id] = nodeData.id;
+			return;
+		}
+
 		const newNodeData = structuredClone(nodeData);
 		if (newNodeData.type === 'OutputNode') {
 			newNodeData.extras.channel = 1;
 		}
+
 		newNodeData.id += STEREO_PREFIX;
 		graphRegistry.nodes.add(newNodeData);
+		idsMap[nodeData.id] = newNodeData.id;
 	});
+
 	graphRegistry.connections.values().forEach((connectionData) => {
 		const newConnectionData = structuredClone(connectionData);
 		newConnectionData.id = createId();
-		newConnectionData.targetNodeId += STEREO_PREFIX;
-		newConnectionData.inputPath.nodeId += STEREO_PREFIX;
+		newConnectionData.targetNodeId = idsMap[connectionData.targetNodeId];
+		newConnectionData.inputPath.nodeId = idsMap[connectionData.inputPath.nodeId];
 		graphRegistry.connections.add(newConnectionData);
 	});
 }
