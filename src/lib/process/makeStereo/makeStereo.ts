a2de5beaa35a4be2d8d685dc522d363b835bd9ef
Yolisses <yolisses0@gmail.com>
2025-08-23 10:07:23 -0300

    Add comment in makeStereo

--- src/lib/process/makeStereo/makeStereo.ts
+++ src/lib/process/makeStereo/makeStereo.ts
@@ -3,6 +3,17 @@ import type { GraphRegistry } from '$lib/graph/GraphRegistry';
 
 const STEREO_PREFIX = '/channel1';
 
+// To make the graph stereo, the ideal solution from this end would be all nodes
+// having two outputs, but that seems such a hard coded waste. Mono instruments
+// or effects (without randomness) would be all calculating the same thing twice
+// for no reason.
+//
+// That said, the nodes only will be duplicated if they have to. For simple
+// graphs it means duplicating every node that differs from one channel to
+// another (both random nodes and nodes that depend from random nodes). But for
+// graphs with nested modules, it is required to use a "meta" node that will be
+// equivalent to creating a second output for the module nodes. Only modules
+// with stereo output will have this attached to.
 export function makeStereo(graphRegistry: GraphRegistry) {
 	graphRegistry.nodes.values().forEach((nodeData) => {
 		const newNodeData = structuredClone(nodeData);
