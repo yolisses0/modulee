e38557dbc6c0b09fb35cb368048700d07a33ace5
Yolisses <yolisses0@gmail.com>
2025-09-02 10:54:27 -0300

    Fix seedFake.ts

--- src/lib/fake/seedFake.ts
+++ src/lib/fake/seedFake.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { createFakeExternalModule } from './createFakeExternalModule';
 import { createFakeUser } from './createFakeUser';
 import { range } from './range';
