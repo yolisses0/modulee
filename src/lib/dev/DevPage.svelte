e9de17b3fc3fb373acbfc859ef9abb0c4f0e733b
Yolisses <yolisses0@gmail.com>
2025-09-07 02:15:30 -0300

    Create node moving on dev page

--- src/lib/dev/DevPage.svelte
+++ src/lib/dev/DevPage.svelte
@@ -1,11 +1,14 @@
 <script>
+	import Movable from './Movable.svelte';
+
 	function handleClick() {
 		console.log('here');
 	}
 </script>
 
 <div class="h-[300px] w-[300px] overflow-auto border">
-	<div class="h-[600px] w-[600px] touch-pan-x touch-pan-y">
+	<div class="relative h-[600px] w-[600px] touch-pan-x touch-pan-y">
 		<button onclick={handleClick}>click me</button>
+		<Movable />
 	</div>
 </div>
