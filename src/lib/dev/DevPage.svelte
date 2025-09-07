365106d451d4e13b9f3737172bff5200587d04a9
Yolisses <yolisses0@gmail.com>
2025-09-07 01:57:53 -0300

    Start creating minimal example in dev page

--- src/lib/dev/DevPage.svelte
+++ src/lib/dev/DevPage.svelte
@@ -1,1 +1,11 @@
-hello, dev
+<script>
+	function handleClick() {
+		console.log('here');
+	}
+</script>
+
+<div class="h-[300px] w-[300px] overflow-auto border">
+	<div class="h-[600px] w-[600px] touch-pan-x touch-pan-y">
+		<button onclick={handleClick}>click me</button>
+	</div>
+</div>
