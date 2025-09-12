d160ce1763101b81904b20f24ad7217f1e6f2552
Yolisses <yolisses0@gmail.com>
2025-09-12 18:53:01 -0300

    Increment dev page experiments

--- src/lib/dev/Connector.svelte
+++ src/lib/dev/Connector.svelte
@@ -12,36 +12,49 @@
 	let element: HTMLElement;
 
 	function handlePointerDown(e: PointerEvent) {
+		console.log('handlePointerDown');
+		element.releasePointerCapture(e.pointerId);
 		connectionHandler.start = id;
 		connectionHandler.startPosition = new Vector(e.clientX, e.clientY);
-		element.setPointerCapture(e.pointerId);
+		// element.setPointerCapture(e.pointerId);
 		e.stopPropagation();
 	}
 
 	function handlePointerUp(e: PointerEvent) {
+		console.log('handlePointerUp');
 		connectionHandler.end = undefined;
 		connectionHandler.endPosition = undefined;
 		connectionHandler.start = undefined;
 		connectionHandler.startPosition = undefined;
-		element.releasePointerCapture(e.pointerId);
+		// element.releasePointerCapture(e.pointerId);
 		e.stopPropagation();
 	}
 
 	function handlePointerMove(e: PointerEvent) {
+		console.log('handlePointerMove');
 		if (!connectionHandler.start) return;
-		console.log('test1');
-
 		const currentPointerPosition = new Vector(e.clientX, e.clientY);
 		connectionHandler.endPosition = currentPointerPosition;
+	}
+
+	function handlePointerEnter(e: PointerEvent) {
+		console.log('handlePointerEnter');
+	}
+
+	function handleTouchStart(e: TouchEvent) {
+		console.log('handleTouchStart');
+		e.stopImmediatePropagation();
 	}
 </script>
 
 <div
 	bind:this={element}
+	class="w-fit touch-none border"
 	onpointerdown={handlePointerDown}
+	onpointerenter={handlePointerEnter}
 	onpointermove={handlePointerMove}
 	onpointerup={handlePointerUp}
-	class="touch-none"
+	ontouchstart={handleTouchStart}
 >
 	Connector {id}
 </div>
