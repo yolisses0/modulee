f6cef1e7879cbbd855dbd4ef63ba5eebc94142de
Yolisses <yolisses0@gmail.com>
2025-09-13 13:23:53 -0300

    Separate CenterOnInternalModuleChange

--- src/lib/graph/GraphCanvasContainer.svelte
+++ src/lib/graph/GraphCanvasContainer.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
 	import { getRequiredContext } from '$lib/global/getRequiredContext';
-	import { getElementSize } from '$lib/graph/getElementSize';
-	import { internalModuleIdContextKey } from '$lib/module/internalModule/internalModuleIdContext';
 	import { setAddNodeInputContext } from '$lib/node/add/addNodeInputContext';
 	import type { Node } from '$lib/node/Node.svelte';
 	import { getScreenFontSize } from '$lib/space/getScreenFontSize';
 	import { getScreenLineHeight } from '$lib/space/getScreenLineHeight';
 	import { spaceContextKey } from '$lib/space/spaceContext';
-	import { zoomContextKey } from '$lib/space/zoom/zoomContext';
 	import { PointerEventDispatcher, rootElementContextKey } from 'nodes-editor';
-	import { tick, untrack, type Snippet } from 'svelte';
-	import { getNodesAveragePosition } from './getNodesAveragePosition';
+	import { type Snippet } from 'svelte';
+	import { CenterOnInternalModuleChange } from './CenterOnInternalModuleChange.svelte';
 	import { GraphCanvasPointerStrategyFactory } from './GraphCanvasPointerStrategyFactory.svelte';
 	import type { GraphSizer } from './GraphSizer.svelte';
 
@@ -30,56 +27,22 @@
 	const graphCanvasPointerStrategyFactory = new GraphCanvasPointerStrategyFactory();
 
 	/* Resizing */
-	let scrollArea = $state<HTMLElement>();
-	$effect(() => {
-		graphSizer.scrollArea = scrollArea;
-	});
 	$effect(() => {
 		graphSizer.handleNodesUpdate(nodes);
 	});
 	const size = $derived(graphSizer.getSize());
 
-	/* Centering on navigation */
-	const internalModuleIdContext = getRequiredContext(internalModuleIdContextKey);
-
-	const zoomContext = getRequiredContext(zoomContextKey);
-
-	function autoScroll() {
-		if (!scrollArea) return;
-		if (nodes.length === 0) return;
-
-		const { minPosition } = graphSizer;
-		if (!minPosition) return;
-
-		const scrollAreaSize = getElementSize(scrollArea);
-		const averagePosition = getNodesAveragePosition(nodes);
-		const scrollPosition = averagePosition
-			.subtract(minPosition)
-			.multiplyByNumber(zoomContext.zoom)
-			.subtract(scrollAreaSize.divideByNumber(2));
-		scrollArea?.scrollTo({ top: scrollPosition.y, left: scrollPosition.x });
-	}
-
-	$effect(() => {
-		// Only runs when internalModuleId changes
-		internalModuleIdContext.internalModuleId;
-		untrack(() => {
-			// Wait for DOM updates
-			tick().then(() => {
-				autoScroll();
-			});
-		});
-	});
-
 	const addNodeInputContext = $state({});
 	setAddNodeInputContext(addNodeInputContext);
+
+	new CenterOnInternalModuleChange(graphSizer, nodes);
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
 	{onscroll}
-	bind:this={scrollArea}
 	class="flex-1 overflow-scroll"
+	bind:this={graphSizer.scrollArea}
 	style:font-size={getScreenFontSize(spaceContext.space) + 'px'}
 	style:line-height={getScreenLineHeight(spaceContext.space) + 'px'}
 >
