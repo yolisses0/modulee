6013c140d6282064a7ef2bf240a43e3e9d687d43
Yolisses <yolisses0@gmail.com>
2025-09-15 08:49:32 -0300

    Remove GraphCanvasPointerStrategyFactory

--- src/lib/graph/GraphCanvasContainer.svelte
+++ src/lib/graph/GraphCanvasContainer.svelte
@@ -4,10 +4,18 @@
 	import { getScreenFontSize } from '$lib/space/getScreenFontSize';
 	import { getScreenLineHeight } from '$lib/space/getScreenLineHeight';
 	import { spaceContextKey } from '$lib/space/spaceContext';
-	import { PointerEventDispatcher, rootElementContextKey } from 'nodes-editor';
-	import { type Snippet } from 'svelte';
-	import { GraphCanvasPointerStrategyFactory } from './GraphCanvasPointerStrategyFactory.svelte';
+	import {
+		EmptyPointerStrategy,
+		PointerEventDispatcher,
+		previewConnectionContextKey,
+		PreviewConnectionPointerStrategy,
+		rootElementContextKey,
+		SelectionBoxPointerStrategy,
+	} from 'nodes-editor';
+	import { onMount, type Snippet } from 'svelte';
 	import type { GraphSizer } from './GraphSizer.svelte';
+	import { PreviewConnectionEndHandler } from './PreviewConnectionEndHandler';
+	import { setIsOverscrollEnabled } from './setIsOverscrollEnabled';
 
 	interface Props {
 		children: Snippet;
@@ -17,16 +25,90 @@
 	}
 
 	const { graphSizer, children, onscroll, oncontextmenu }: Props = $props();
-	const rootElementContext = getRequiredContext(rootElementContextKey);
-	const spaceContext = getRequiredContext(spaceContextKey);
-
-	/* Pointer events handling */
-	const graphCanvasPointerStrategyFactory = new GraphCanvasPointerStrategyFactory();
 
 	const addNodeInputContext = $state({});
 	setAddNodeInputContext(addNodeInputContext);
 
 	const size = $derived(graphSizer.getSize());
+
+	let isTouchDevice = $state<boolean>();
+	const previewConnectionContext = getRequiredContext(previewConnectionContextKey);
+	const rootElementContext = getRequiredContext(rootElementContextKey);
+	const spaceContext = getRequiredContext(spaceContextKey);
+
+	const previewConnectionEndHandler = new PreviewConnectionEndHandler();
+	onMount(() => {
+		isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+	});
+
+	function selectionBoxPointerCondition(e: PointerEvent) {
+		// Ignore mouse right button, used for adding a node
+		return e.button !== 2;
+	}
+
+	const selectionBoxPointerStrategy = $derived.by(() => {
+		if (isTouchDevice === undefined) return;
+		if (rootElementContext.rootElement === undefined) return;
+		return new SelectionBoxPointerStrategy(
+			rootElementContext.rootElement,
+			isTouchDevice,
+			selectionBoxPointerCondition,
+		);
+	});
+
+	$effect(() => {
+		const { rootElement } = rootElementContext;
+		// Sveltekit doesn't support passing `passive` in a declarative way,
+		// since it has super restrict use cases.
+		rootElement?.addEventListener('touchend', handleTouchEnd, {
+			passive: false,
+		});
+		rootElement?.addEventListener('touchstart', handleTouchStart, {
+			passive: false,
+		});
+		rootElement?.addEventListener('touchmove', handleTouchMove, {
+			passive: false,
+		});
+
+		return () => {
+			rootElement?.removeEventListener('touchend', handleTouchEnd);
+			rootElement?.removeEventListener('touchmove', handleTouchMove);
+			rootElement?.removeEventListener('touchstart', handleTouchStart);
+		};
+	});
+
+	const pointerStrategy = $derived.by(() => {
+		const previewConnectionPointerStrategy = new PreviewConnectionPointerStrategy(
+			previewConnectionEndHandler.handleEndPreviewConnection,
+		);
+
+		const pointerStrategy = previewConnectionContext.startConnector
+			? previewConnectionPointerStrategy
+			: selectionBoxPointerStrategy;
+
+		return pointerStrategy ?? new EmptyPointerStrategy();
+	});
+
+	const handleTouchStart = (e: TouchEvent) => {
+		setIsOverscrollEnabled(true);
+		if (selectionBoxPointerStrategy?.isActive) {
+			e.preventDefault();
+		}
+	};
+
+	const handleTouchEnd = (e: TouchEvent) => {
+		setIsOverscrollEnabled(false);
+		if (selectionBoxPointerStrategy?.isActive) {
+			e.preventDefault();
+		}
+	};
+
+	// Maybe it's not required
+	const handleTouchMove = (e: TouchEvent) => {
+		if (selectionBoxPointerStrategy?.isActive) {
+			e.preventDefault();
+		}
+	};
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
@@ -37,7 +119,7 @@
 	style:font-size={getScreenFontSize(spaceContext.space) + 'px'}
 	style:line-height={getScreenLineHeight(spaceContext.space) + 'px'}
 >
-	<PointerEventDispatcher pointerStrategy={graphCanvasPointerStrategyFactory.getPointerStrategy()}>
+	<PointerEventDispatcher {pointerStrategy}>
 		<div
 			{oncontextmenu}
 			bind:this={rootElementContext.rootElement}
