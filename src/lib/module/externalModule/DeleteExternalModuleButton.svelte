265ae1655d3177c1a653f81e2baaf96851051ff4
Yolisses <yolisses0@gmail.com>
2025-09-20 08:44:40 -0300

    Hide delete external module button for non owner user

--- src/lib/module/externalModule/DeleteExternalModuleButton.svelte
+++ src/lib/module/externalModule/DeleteExternalModuleButton.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { getRequiredContext } from '$lib/global/getRequiredContext';
 	import { ModalState } from '$lib/project/ui/ModalState.svelte';
+	import { userDataContextKey } from '$lib/user/userDataContext';
 	import { faTrash } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 	import DeleteExternalModuleModal from './DeleteExternalModuleModal.svelte';
@@ -11,12 +13,15 @@
 
 	const { externalModuleData }: Props = $props();
 	const modalState = new ModalState();
+	const userDataContext = getRequiredContext(userDataContextKey);
 </script>
 
-<button class="common-button center-content" onclick={modalState.open}>
-	<Fa fw icon={faTrash} />
-	Delete
-</button>
+{#if userDataContext.userData.id === externalModuleData.userId}
+	<button class="common-button center-content" onclick={modalState.open}>
+		<Fa fw icon={faTrash} />
+		Delete
+	</button>
+{/if}
 
 {#if modalState.isOpen}
 	<DeleteExternalModuleModal {externalModuleData} {modalState} />
