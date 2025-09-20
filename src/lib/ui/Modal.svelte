aa54d38fcc7c87a019318e7d46cf08c66771cd2e
Yolisses <yolisses0@gmail.com>
2025-09-20 08:26:59 -0300

    Remove old TODO in Modal

--- src/lib/ui/Modal.svelte
+++ src/lib/ui/Modal.svelte
@@ -25,7 +25,6 @@
 	}
 </script>
 
-<!-- TODO consider replacing closeModal by making isOpen bindable -->
 <div class="pointer-events-none fixed inset-0 flex items-center justify-center bg-black/50">
 	<div class="pointer-events-auto contents" bind:this={modal}>
 		{@render children?.()}
