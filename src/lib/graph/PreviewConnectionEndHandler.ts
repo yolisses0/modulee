5ce6c9b303d9fa7f1ba8e1a4dbbb08e6c2a71e75
Yolisses <yolisses0@gmail.com>
2025-09-24 06:58:27 -0300

    Refactor PreviewConnectionEndHandler

--- src/lib/graph/PreviewConnectionEndHandler.ts
+++ src/lib/graph/PreviewConnectionEndHandler.ts
@@ -3,10 +3,12 @@ import { SetConnectionCommand } from '$lib/commands/connection/SetConnectionComm
 import { editorContextKey } from '$lib/editor/editorContext';
 import { createId } from '$lib/global/createId';
 import { getRequiredContext } from '$lib/global/getRequiredContext';
+import type { Input } from '$lib/input/Input';
 import type { InputPath } from '$lib/input/InputPath';
 import { addNodeMenuParamsContextKey } from '$lib/node/add/addNodeMenuParamsContext';
 import { getInputAndOutput } from '$lib/node/getInputAndOutput';
 import { NODE_ITEM_WIDTH } from '$lib/node/NODE_ITEM_WIDTH';
+import type { Output } from '$lib/node/Output';
 import { projectDataContextKey } from '$lib/project/ui/projectDataContext';
 import { spaceContextKey } from '$lib/space/spaceContext';
 import { Vector, type EndPreviewConnectionEvent } from 'nodes-editor';
@@ -27,37 +29,47 @@ export class PreviewConnectionEndHandler {
 		};
 
 		if (output) {
-			const command = new SetConnectionCommand({
-				id: createId(),
-				type: 'SetConnectionCommand',
-				createdAt: new Date().toJSON(),
-				projectId: this.projectDataContext.projectData.id,
-				details: {
-					connection: {
-						inputPath,
-						id: createId(),
-						targetNodeId: output?.node.id,
-					},
-				},
-			});
-			this.editorContext.editor.execute(command);
+			this.connectNode(inputPath, output);
 		} else {
-			const command = new DisconnectCommand({
-				id: createId(),
-				details: { inputPath },
-				type: 'DisconnectCommand',
-				createdAt: new Date().toJSON(),
-				projectId: this.projectDataContext.projectData.id,
-			});
-			this.editorContext.editor.execute(command);
-
-			const screenPosition = e.mousePosition;
-			let dataPosition = this.spaceContext.space.getDataPosition(screenPosition);
-			dataPosition = dataPosition.subtract(new Vector(NODE_ITEM_WIDTH - 1, 0));
-			this.addNodeMenuParamsContext.addNodeMenuParams = {
-				input,
-				position: dataPosition,
-			};
+			this.disconnectNode(inputPath, input, e.mousePosition);
 		}
 	};
+
+	connectNode(inputPath: InputPath, output: Output) {
+		const command = new SetConnectionCommand({
+			createdAt: new Date().toJSON(),
+			details: {
+				connection: {
+					id: createId(),
+					inputPath,
+					targetNodeId: output?.node.id,
+				},
+			},
+			id: createId(),
+			projectId: this.projectDataContext.projectData.id,
+			type: 'SetConnectionCommand',
+		});
+		this.editorContext.editor.execute(command);
+	}
+
+	disconnectNode(inputPath: InputPath, input: Input, mousePosition: Vector) {
+		const command = new DisconnectCommand({
+			createdAt: new Date().toJSON(),
+			details: {
+				inputPath,
+			},
+			id: createId(),
+			projectId: this.projectDataContext.projectData.id,
+			type: 'DisconnectCommand',
+		});
+		this.editorContext.editor.execute(command);
+
+		const screenPosition = mousePosition;
+		let dataPosition = this.spaceContext.space.getDataPosition(screenPosition);
+		dataPosition = dataPosition.subtract(new Vector(NODE_ITEM_WIDTH - 1, 0));
+		this.addNodeMenuParamsContext.addNodeMenuParams = {
+			input,
+			position: dataPosition,
+		};
+	}
 }
