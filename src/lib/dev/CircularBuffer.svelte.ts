93f5b9fd4d613d8648f5463bf94583a75e75672d
Yolisses <yolisses0@gmail.com>
2025-09-29 17:23:47 -0300

    Simplify CircularBuffer

--- src/lib/dev/CircularBuffer.svelte.ts
+++ src/lib/dev/CircularBuffer.svelte.ts
@@ -1,7 +1,6 @@
 export class CircularBuffer {
 	public buffer: number[] = $state([]);
 	private size: number;
-	private head: number = 0;
 	private tail: number = 0;
 
 	constructor(size: number) {
@@ -12,6 +11,5 @@ export class CircularBuffer {
 	push(item: number): void {
 		this.buffer[this.tail] = item;
 		this.tail = (this.tail + 1) % this.size;
-		this.head = (this.head + 1) % this.size;
 	}
 }
