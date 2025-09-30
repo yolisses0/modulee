513b9cc722654fd63b62ceac5cf70229b41865fe
Yolisses <yolisses0@gmail.com>
2025-09-30 18:35:23 -0300

    Instantiate JuceOscilloscopeBackend in initializeJuceAudioFeatures

--- src/lib/audio/initializeJuceAudioFeatures.ts
+++ src/lib/audio/initializeJuceAudioFeatures.ts
@@ -2,20 +2,26 @@ import { getRequiredContext } from '$lib/global/getRequiredContext';
 import { activePitchesContextKey } from '$lib/piano/activePitchesContext';
 import { audioBackendContextKey } from './audioBackendContext';
 import { JuceAudioBackend } from './JuceAudioBackend';
+import { JuceOscilloscopeBackend } from './JuceOscilloscopeBackend';
+import { oscilloscopeBackendContextKey } from './oscilloscopeBackendContext';
 import { VirtualPianoMidiBackend } from './VirtualPianoMidiBackend';
 
 export function initializeJuceAudioFeatures() {
 	const audioBackendContext = getRequiredContext(audioBackendContextKey);
 	const activePitchesContext = getRequiredContext(activePitchesContextKey);
+	const oscilloscopeBackendContext = getRequiredContext(oscilloscopeBackendContextKey);
 
-	const audioBackend = new JuceAudioBackend();
-	audioBackendContext.audioBackend = audioBackend;
+	const juceAudioBackend = new JuceAudioBackend();
+	audioBackendContext.audioBackend = juceAudioBackend;
 
 	const virtualPianoMidiBackend = new VirtualPianoMidiBackend(
-		audioBackend,
+		juceAudioBackend,
 		activePitchesContext.activePitches,
 	);
 	virtualPianoMidiBackend.initialize();
+
+	const juceOscilloscopeBackend = new JuceOscilloscopeBackend();
+	oscilloscopeBackendContext.oscilloscopeBackend = juceOscilloscopeBackend;
 
 	return () => {
 		virtualPianoMidiBackend.destroy();
