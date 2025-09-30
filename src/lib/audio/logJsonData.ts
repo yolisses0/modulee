f7a4af5bdf233e74d5c45c9539cf499e303a2ed0
Yolisses <yolisses0@gmail.com>
2025-09-29 21:54:52 -0300

    Delete unused logJsonData

--- src/lib/audio/logJsonData.ts
+++
@@ -1,7 +0,0 @@
-import sortJson from 'sort-json';
-
-export function logJsonData(data: unknown) {
-	const sortedInternalModulesEngineData = sortJson(data);
-	const dataJson = JSON.stringify(sortedInternalModulesEngineData, undefined, 2);
-	console.debug(dataJson);
-}
