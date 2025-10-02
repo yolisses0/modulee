6c83b489a222dd0d647a497ae83e490699a427bc
Yolisses <yolisses0@gmail.com>
2025-10-01 23:16:56 -0300

    Add contact info on about page

--- src/lib/about/AboutText.svelte
+++ src/lib/about/AboutText.svelte
@@ -1,3 +1,9 @@
+<script>
+	import { faDiscord, faReddit, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+	import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+</script>
+
 <p>
 	Modulee is a modular synthesizer. It makes easy to create and share instruments and effects for
 	music production. It is free and open source.
@@ -5,3 +11,24 @@
 <p><a href="/privacy-policy">Privacy Policy</a></p>
 <p><a href="/terms-of-use">Terms of Use</a></p>
 <p><a href="https://github.com/yolisses0/modulee" target="_blank">Github</a></p>
+<br />
+<div>
+	<div>You can contact me on:</div>
+	<a href="https://www.reddit.com/user/yolisses/" about="_blank" class="flex items-center gap-1">
+		<Fa icon={faReddit} fw />
+		Reddit: yolisses
+	</a>
+	<a href="mailto:yolisses0@gmail.com" about="_blank" class="flex items-center gap-1">
+		<Fa icon={faEnvelope} fw />
+		Email: yolisses0@gmail.com
+	</a>
+	<a href="https://wa.me/5583993757502" about="_blank" class="flex items-center gap-1">
+		<Fa icon={faWhatsapp} fw />
+		WhatsApp: +55 83 99375-7502
+	</a>
+	<a href="https://discord.gg/J9n2SuBe" about="_blank" class="flex items-center gap-1">
+		<Fa icon={faDiscord} fw />
+		Discord: yolisses0
+	</a>
+	<div>GMT -03:00 (Brazil)</div>
+</div>
