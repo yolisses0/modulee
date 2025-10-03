08b706ffb1757fbfc54f4b9a8757fe6a6cc69990
Yolisses <yolisses0@gmail.com>
2025-10-03 07:44:05 -0300

    Fix ProjectNavbar mobile layout

--- src/lib/project/ui/ProjectNavbar.svelte
+++ src/lib/project/ui/ProjectNavbar.svelte
@@ -39,12 +39,13 @@
 		<Fa fw icon={faFileAlt} />
 		<div class="navbar-item-text">Project</div>
 	</a>
-	<div class="navbar-item-text flex-1"></div>
+	<div class="flex-1 max-md:hidden"></div>
 	<a href="/projects" aria-label="Home" title="Go to projects page" class="navbar-item">
 		<picture>
 			<source media="(max-width: 48rem)" srcset="/logo.svg" />
 			<source media="(min-width: 48.0625rem)" srcset="/logo-with-name-beta.svg" />
 			<img height="16" alt="Modulee logo" style="height: 1rem;" src="/logo.svg" />
 		</picture>
+		<div class="navbar-item-text">Modulee</div>
 	</a>
 </div>
