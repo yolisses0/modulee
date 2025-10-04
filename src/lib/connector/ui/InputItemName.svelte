6ef5a1389eb6ec167e7cfd62e96370b1d23009a4
Yolisses <yolisses0@gmail.com>
2025-10-04 14:54:43 -0300

    Add target node name indicator on input item

--- src/lib/connector/ui/InputItemName.svelte
+++ src/lib/connector/ui/InputItemName.svelte
@@ -13,7 +13,14 @@
 	title={input.name}
 	style:padding-left="0.3lh"
 	class:italic={input instanceof ModuleNodeAudioInput}
-	class="overflow-and-ellipsis block flex-1 overflow-hidden text-start italic hover:bg-white/10"
+	class="flex flex-1 overflow-hidden text-start italic hover:bg-white/10"
 >
-	{input.name}
+	<div class="overflow-and-ellipsis">
+		{input.name}
+	</div>
+	{#if input.targetNode?.name}
+		<div class="overflow-and-ellipsis text-end opacity-25" style:padding-right="0.2lh">
+			{input.targetNode.name}
+		</div>
+	{/if}
 </button>
