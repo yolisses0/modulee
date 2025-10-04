2dc29b38a55a72f64ffe2f58336e3f1d202f017a
Yolisses <yolisses0@gmail.com>
2025-10-03 21:42:21 -0300

    Remove one console.log

--- src/lib/dev/DevPage.svelte
+++ src/lib/dev/DevPage.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import '../juce/index';
-	import { getNativeFunction } from '../juce/index';
-
-	onMount(() => {
-		const getOscilloscopeData = getNativeFunction('getOscilloscopeData');
-		getOscilloscopeData().then(console.log);
-	});
 </script>
 
 hello dev
