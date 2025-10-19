eb25f54437dec5b623e754130b98e6c2c4a436bd
Yolisses <yolisses0@gmail.com>
2025-10-19 17:47:41 -0300

    Add test involving input nodes in flattenModuleNodes

--- src/lib/process/flattenModuleNodes/flattenModuleNodes.test.ts
+++ src/lib/process/flattenModuleNodes/flattenModuleNodes.test.ts
@@ -7,10 +7,7 @@ import { flattenModuleNodes } from './flattenModuleNodes';
 function mockCreateId() {
 	vi.mock('$lib/global/createId', () => {
 		let counter = 1;
-		return {
-			counter,
-			createId: () => 'newId' + counter++,
-		};
+		return { counter, createId: () => 'newId' + counter++ };
 	});
 }
 
@@ -56,7 +53,7 @@ describe('flattenModuleNodes', () => {
 				{ id: 'node2', internalModuleId: 'module2' },
 				{ id: 'node3', internalModuleId: 'module2' },
 			],
-			connections: [{ id: 'connection2', inputPath: { nodeId: 'node3' }, targetNodeId: 'node2' }],
+			connections: [{ id: 'connection1', inputPath: { nodeId: 'node3' }, targetNodeId: 'node2' }],
 		} as GraphData;
 		const graphRegistry = getGraphRegistry(graphData, []);
 
@@ -73,8 +70,61 @@ describe('flattenModuleNodes', () => {
 			],
 			connections: [
 				//
-				{ id: 'connection2', inputPath: { nodeId: 'node3' }, targetNodeId: 'node2' },
+				{ id: 'connection1', inputPath: { nodeId: 'node3' }, targetNodeId: 'node2' },
 				{ id: 'newId3', inputPath: { nodeId: 'newId2' }, targetNodeId: 'newId1' },
+			],
+		} as GraphData);
+	});
+
+	it('replaces module nodes by the module nodes and replaces input nodes', () => {
+		mockCreateId();
+		const graphData = {
+			mainInternalModuleId: 'module1',
+			internalModules: [{ id: 'module1' }, { id: 'module2' }],
+			nodes: [
+				{ id: 'node1', internalModuleId: 'module1' },
+				{
+					id: 'node2',
+					internalModuleId: 'module1',
+					type: 'ModuleNode',
+					extras: { moduleReference: { moduleId: 'module2' } },
+				},
+				{ id: 'node3', internalModuleId: 'module2', type: 'InputNode' },
+				{ id: 'node4', internalModuleId: 'module2' },
+			],
+			connections: [
+				{
+					id: 'connection1',
+					inputPath: { nodeId: 'node2', inputKey: 'node3' },
+					targetNodeId: 'node1',
+				},
+				{
+					id: 'connection2',
+					inputPath: { nodeId: 'node4', inputKey: 'input1' },
+					targetNodeId: 'node3',
+				},
+			],
+		} as GraphData;
+		const graphRegistry = getGraphRegistry(graphData, []);
+
+		flattenModuleNodes(graphRegistry);
+
+		expect(getGraphData(graphRegistry)).toEqual({
+			mainInternalModuleId: 'module1',
+			internalModules: [{ id: 'module1' }, { id: 'module2' }],
+			nodes: [
+				{ id: 'node1', internalModuleId: 'module1' },
+				{ id: 'node3', internalModuleId: 'module2', type: 'InputNode' },
+				{ id: 'node4', internalModuleId: 'module2' },
+				{ id: 'newId4', internalModuleId: 'module1' },
+			],
+			connections: [
+				{ id: 'connection2', inputPath: { nodeId: 'node4' }, targetNodeId: 'node3' },
+				{
+					id: 'newId5',
+					inputPath: { nodeId: 'newId4', inputKey: 'input1' },
+					targetNodeId: 'node1',
+				},
 			],
 		} as GraphData);
 	});
