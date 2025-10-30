ed861f96f8823c6493b8150326389119aaaaaeb5
Yolisses <yolisses0@gmail.com>
2025-10-30 07:32:49 -0300

    Start using composite pattern on flattenModuleNodes

--- src/lib/process/flattenModuleNodes/flattenModuleNodes.ts
+++ src/lib/process/flattenModuleNodes/flattenModuleNodes.ts
@@ -1,109 +1,89 @@
 import type { ConnectionData } from '$lib/connection/ConnectionData';
 import { ById } from '$lib/editor/ById';
 import type { GraphRegistry } from '$lib/graph/GraphRegistry';
+import type { InternalModuleData } from '$lib/module/internalModule/InternalModuleData';
 import type { NodeData } from '$lib/node/data/NodeData';
-import type { ModuleNodeData } from '$lib/node/data/variants/ModuleNodeData';
-import type { ModuleVoicesNodeData } from '$lib/node/data/variants/ModuleVoicesNodeData';
 
-class FlattingModule {
+class FlattingConnection {
 	constructor(
 		public graphRegistry: GraphRegistry,
-		public internalModuleId: string,
+		public connectionData: ConnectionData,
 	) {}
 
-	// TODO rename
-	doTheThing() {
-		const nodes = this.graphRegistry.nodes
+	flatten(result: GraphRegistry) {
+		result.connections.add(this.connectionData);
+	}
+}
+
+class FlattingNode {
+	connections: FlattingConnection[];
+
+	constructor(
+		public graphRegistry: GraphRegistry,
+		public nodeData: NodeData,
+	) {
+		this.connections = graphRegistry.connections
 			.values()
-			.filter((nodeData) => {
-				return nodeData.internalModuleId === this.internalModuleId;
-			})
-			.map((value) => structuredClone(value));
+			.filter((connectionData) => connectionData.inputPath.nodeId === nodeData.id)
+			.map((connectionData) => new FlattingConnection(graphRegistry, connectionData));
+	}
 
-		const nodesById = ById.fromItems(nodes);
-
-		const connections = this.graphRegistry.connections
-			.values()
-			.filter((connectionData) => {
-				return (
-					nodesById.containsId(connectionData.inputPath.nodeId) &&
-					nodesById.containsId(connectionData.targetNodeId)
-				);
-			})
-			.map((value) => structuredClone(value));
-
-		const connectionsById = ById.fromItems(connections);
-
-		nodes.forEach((nodeData) => {
-			if (nodeData.type === 'ModuleVoicesNode') {
-				this.doTheThingForModuleVoicesNode(nodeData, nodesById, connectionsById);
-			}
-			if (nodeData.type === 'ModuleNode') {
-				this.doTheThingForModuleNode(
-					nodeData,
-					nodesById,
-					connectionsById,
-					nodeData.internalModuleId,
-				);
-			}
+	flatten(result: GraphRegistry) {
+		result.nodes.add(this.nodeData);
+		this.connections.forEach((connection) => {
+			connection.flatten(result);
 		});
+	}
+}
 
-		return {
-			nodes: nodesById,
-			connections: connectionsById,
+class FlattingModule {
+	nodes: FlattingNode[];
+
+	constructor(
+		public graphRegistry: GraphRegistry,
+		public moduleData: InternalModuleData,
+	) {
+		this.nodes = graphRegistry.nodes
+			.values()
+			.filter((nodeData) => nodeData.internalModuleId === moduleData.id)
+			.map((nodeData) => new FlattingNode(graphRegistry, nodeData));
+	}
+
+	flatten(result: GraphRegistry) {
+		this.graphRegistry.internalModules.add(this.moduleData);
+		this.nodes.forEach((node) => {
+			node.flatten(result);
+		});
+	}
+}
+
+class FlatteningGraph {
+	modules: FlattingModule[];
+
+	constructor(public graphRegistry: GraphRegistry) {
+		this.modules = graphRegistry.internalModules
+			.values()
+			.map((moduleData) => new FlattingModule(graphRegistry, moduleData));
+	}
+
+	flatten(): GraphRegistry {
+		const result: GraphRegistry = {
+			connections: new ById(),
+			externalModules: new ById(),
+			internalModules: new ById(),
+			mainInternalModuleId: this.graphRegistry.mainInternalModuleId,
+			nodes: new ById(),
 		};
-	}
 
-	private doTheThingForModuleNode(
-		nodeData: ModuleNodeData,
-		nodesById: ById<NodeData>,
-		connectionsById: ById<ConnectionData>,
-		newModuleId: string,
-	) {
-		const moduleId = nodeData.extras.moduleReference?.moduleId;
-
-		if (!moduleId) return;
-		if (!this.graphRegistry.internalModules.containsId(moduleId)) return;
-
-		const flattingModule = new FlattingModule(this.graphRegistry, moduleId);
-
-		const result = flattingModule.doTheThing();
-		result.nodes.values().forEach((nodeData) => {
-			nodeData.id += '_copy_to_' + newModuleId;
-		});
-		result.connections.values().forEach((connection) => {
-			connection.id += '_copy_to_' + newModuleId;
-			connection.targetNodeId += '_copy_to_' + newModuleId;
-			connection.inputPath.nodeId += '_copy_to_' + newModuleId;
+		this.modules.forEach((module) => {
+			module.flatten(result);
 		});
 
-		nodesById.addMany(result.nodes.values());
-		connectionsById.addMany(result.connections.values());
-	}
-
-	private doTheThingForModuleVoicesNode(
-		nodeData: ModuleVoicesNodeData,
-		nodesById: ById<NodeData>,
-		connectionsById: ById<ConnectionData>,
-	) {
-		const moduleId = nodeData.extras.moduleReference?.moduleId;
-
-		if (!moduleId) return;
-
-		const flattingModule = new FlattingModule(this.graphRegistry, moduleId);
-
-		const result = flattingModule.doTheThing();
-		nodesById.addMany(result.nodes.values());
-		connectionsById.addMany(result.connections.values());
+		return result;
 	}
 }
 
 export function flattenModuleNodes(graphRegistry: GraphRegistry) {
-	const flattingModule = new FlattingModule(graphRegistry, graphRegistry.mainInternalModuleId);
-	const { nodes, connections } = flattingModule.doTheThing();
-
-	graphRegistry.nodes = nodes;
-	graphRegistry.connections = connections;
-
-	console.log(graphRegistry);
+	const flatteningGraph = new FlatteningGraph(graphRegistry);
+	flatteningGraph.flatten();
 }
