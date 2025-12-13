869cd7d062285e296572ae051b74f99a235ec22c
Yolisses <iamurise@gmail.com>
2025-12-13 04:40:18 -0300

    Create /u/[username] route

---
+++ src/routes/(home)/u/[username]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { getRequiredContext } from '$lib/global/getRequiredContext';
+	import { homeNavbarSelectionContextKey } from '$lib/home/homeNavbarSelectionContext';
+	import UserPage from '$lib/user/UserPage.svelte';
+	import type { PageData } from './$types';
+
+	interface Props {
+		data: PageData;
+	}
+
+	const { data }: Props = $props();
+
+	getRequiredContext(homeNavbarSelectionContextKey).homeNavbarSelection = 'user';
+</script>
+
+<UserPage userData={data.userData} />
